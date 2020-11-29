3107e90e8e39f560089451280912a222d5528cc6
runner <runner@Mac-1606610217975.local>
2020-11-29 00:47:56 +0000

    version 0.1.71 of myci.rb

---
+++ myci.rb
@@ -0,0 +1,15 @@
+class Myci < Formula
+  desc "Helper CI scripts."
+  homepage "https://github.com/igagis/myci"
+  url "https://github.com/igagis/myci/archive/0.1.71.tar.gz"
+  sha256 "85e9de18465d75ee4d713685afb8ba69d9c65fe79ee32db55b3d4de9a69c96f9"
+
+  def install
+    ENV['PATH'] += ':/usr/local/bin'
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+  
+  test do
+    system "make", "test"
+  end
+end
