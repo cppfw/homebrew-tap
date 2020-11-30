c9e9a20ab4fc206d83be9da9674a0abb1476e660
runner <runner@Mac-1606747675224.local>
2020-11-30 14:55:18 +0000

    version 1.0.41 of libopros.rb

---
+++ libopros.rb
@@ -0,0 +1,18 @@
+class Libopros < Formula
+  desc "C++ cross-platform event waiting library."
+  homepage "https://github.com/cppfw/opros"
+  url "https://github.com/cppfw/opros/archive/1.0.41.tar.gz"
+  sha256 "07debb101c2c4ec81fce50aa82a2c52d76bffdd1cee0502aa0c6f962c740e864"
+
+  depends_on "prorab" => :build
+  depends_on "libutki"
+
+  def install
+    ENV['PATH'] += ':/usr/local/bin'
+    system "make", "-f", "src/makefile", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    system "make", "test"
+  end
+end
