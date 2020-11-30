e35a1d003dd7deae59f36194bb112eb15efa10bc
runner <runner@Mac-1606768758563.local>
2020-11-30 20:46:18 +0000

    version 0.2.12 of libclargs.rb

---
+++ libclargs.rb
@@ -0,0 +1,20 @@
+class Libclargs < Formula
+  desc "C++ cross-platform library for parsing command line arguments."
+  homepage "https://github.com/cppfw/clargs"
+  url "https://github.com/cppfw/clargs/archive/0.2.12.tar.gz"
+  sha256 "42b41c0aa61ad7c1b9b060b8c625e7b1372c547b1464453e2c29814ce5141361"
+
+  depends_on "prorab" => :build
+  depends_on "prorab-extra" => :build
+  depends_on "myci" => :build
+  depends_on "libutki"
+
+  def install
+    ENV['PATH'] += ':/usr/local/bin'
+    system "make", "-C", "src", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    system "make", "test"
+  end
+end
