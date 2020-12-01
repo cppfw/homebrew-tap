1c3d1f5cafbad00dd5389dc3ae8ee4dd94188468
runner <runner@Mac-1606836299791.local>
2020-12-01 15:35:22 +0000

    version 0.5.145 of libmorda.rb

---
+++ libmorda.rb
@@ -0,0 +1,32 @@
+class Libmorda < Formula
+  desc "C++ cross-platform OpenGL based GUI library."
+  homepage "https://github.com/cppfw/morda"
+  url "https://github.com/cppfw/morda/archive/0.5.145.tar.gz"
+  sha256 "bf4e2f1b56c5c0ea332c7e8befe6198a493c4246229ce0015905fb56e2fa821f"
+
+  depends_on "prorab" => :build
+  depends_on "prorab-extra" => :build
+  depends_on "myci" => :build
+  depends_on "glew" => :build
+  depends_on "libnitki" => :build
+  depends_on "libutki"
+  depends_on "libpapki"
+  depends_on "libpuu"
+  depends_on "libpng"
+  depends_on "jpeg"
+  depends_on "z"
+  depends_on "freetype"
+  depends_on "libr4"
+  depends_on "libsvgdom"
+  depends_on "libsvgren"
+  
+  def install
+    ENV['PATH'] += ':/usr/local/bin'
+    system "make", "-f", "src/morda/makefile", "install", "PREFIX=#{prefix}"
+    system "make", "-f", "res/makefile", "install", "PREFIX=#{prefix}"
+  end
+
+  #test do
+  #  system "make", "test"
+  #end
+end
