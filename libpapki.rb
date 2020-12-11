3e2ffeef623418f3463f29b7fcd2a1c4a9d5a70e
runner <runner@Mac-1607701643052.local>
2020-12-11 15:49:43 +0000

    version 1.0.87 of libpapki.rb

--- libpapki.rb
+++ libpapki.rb
@@ -1,8 +1,8 @@
 class Libpapki < Formula
   desc "C++ cross-platform file system library."
   homepage "https://github.com/cppfw/papki"
-  url "https://github.com/cppfw/papki/archive/1.0.86.tar.gz"
-  sha256 "93831e1e4d4f971534af40e149d0e93aad4443d3029c7837557bdbc3ff24ecc5"
+  url "https://github.com/cppfw/papki/archive/1.0.87.tar.gz"
+  sha256 "10a78df55743f5473a11e28d003ead46b665825a7300cef160453c3a4ca470c6"
 
   depends_on "prorab" => :build
   depends_on "libutki"
