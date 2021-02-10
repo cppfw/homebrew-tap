569f411080eb367717d52d26a63efd8273f0b354
runner <runner@Mac-1612959415844.local>
2021-02-10 12:25:29 +0000

    version 0.5.7 of libsvgren.rb

--- libsvgren.rb
+++ libsvgren.rb
@@ -1,8 +1,8 @@
 class Libsvgren < Formula
   desc "C++ cross-platform SVG rendering library."
   homepage "https://github.com/igagis/svgren"
-  url "https://github.com/igagis/svgren/archive/0.5.6.tar.gz"
-  sha256 "5b845116ad715a51a6bd5f498251679838c84803aa9af20ecbd459a15a3e20ce"
+  url "https://github.com/igagis/svgren/archive/0.5.7.tar.gz"
+  sha256 "fe3e33399d0280d8911238eea72dd734df9464befe9de94a405da4a246812532"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
