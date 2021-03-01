277653f6a639bf75c6bb03488227bc70f3281930
runner <runner@Mac-1614621074497.local>
2021-03-01 17:54:17 +0000

    version 1.1.114 of libutki.rb

--- libutki.rb
+++ libutki.rb
@@ -1,8 +1,8 @@
 class Libutki < Formula
   desc "C++ utility functions library. Stuff missing from std:: namespace."
   homepage "https://github.com/cppfw/utki"
-  url "https://github.com/cppfw/utki/archive/1.1.113.tar.gz"
-  sha256 "acc377db08303341135a4c99b90664b6b95208efcd1a805c9ccf812d7dfdd23e"
+  url "https://github.com/cppfw/utki/archive/1.1.114.tar.gz"
+  sha256 "4fb7cce93f7a6fcf30cf883afdfe8f331794074e1841ea757a10f976477ef0f5"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
