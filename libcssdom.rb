792d86a885f7627d47744da95d73382e56be4dd4
runner <runner@Mac-1614721002641.local>
2021-03-02 21:44:52 +0000

    version 0.1.15 of libcssdom.rb

--- libcssdom.rb
+++ libcssdom.rb
@@ -1,8 +1,8 @@
 class Libcssdom < Formula
   desc "C++ cross-platform CSS DOM library."
   homepage "https://github.com/cppfw/cssdom"
-  url "https://github.com/cppfw/cssdom/archive/0.1.14.tar.gz"
-  sha256 "c452487e46ce9266108520ed629fc5225ecd656e2afac227073ea7872c6993dd"
+  url "https://github.com/cppfw/cssdom/archive/0.1.15.tar.gz"
+  sha256 "53f98130ba94c069b77b9f569163405ee911a857aaa0333de391b6bc9264a10d"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
