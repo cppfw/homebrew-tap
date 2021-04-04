2f798ed47fee704756b7c5212051c54b46901b61
runner <runner@Mac-1617533659630.local>
2021-04-04 12:01:17 +0000

    version 0.1.18 of libcssom.rb

--- libcssom.rb
+++ libcssom.rb
@@ -1,8 +1,8 @@
 class Libcssom < Formula
   desc "C++ cross-platform CSS DOM library."
   homepage "https://github.com/cppfw/cssom"
-  url "https://github.com/cppfw/cssom/archive/0.1.17.tar.gz"
-  sha256 "892516dc2eb68e830acd9d9a1e4caf8a59e7b8ae3171e36ba6eb0180f8c71e10"
+  url "https://github.com/cppfw/cssom/archive/0.1.18.tar.gz"
+  sha256 "22b461dbd2c05bbcbc503db309bab4bd54fcefb968115b0b0e2ac074b28882a4"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
