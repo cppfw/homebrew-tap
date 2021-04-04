a3c83fdc3c9b8d7fc41879f6af9ce0cf434d345e
runner <runner@Mac-1617542568276.local>
2021-04-04 13:33:08 +0000

    version 0.3.60 of libsvgdom.rb

--- libsvgdom.rb
+++ libsvgdom.rb
@@ -1,15 +1,15 @@
 class Libsvgdom < Formula
   desc "C++ cross-platform SVG DOM library."
   homepage "https://github.com/cppfw/svgdom"
-  url "https://github.com/cppfw/svgdom/archive/0.3.59.tar.gz"
-  sha256 "5879e8ead27e27045e0038d938e6fff7c58f039729a2fce7a93eebca6de4265d"
+  url "https://github.com/cppfw/svgdom/archive/0.3.60.tar.gz"
+  sha256 "4809ac0efb2f74e8e7369e25a6c19a65b65092891e9d7e436882911c68766966"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
   depends_on "myci" => :build
   depends_on "pkg-config" => :build
   depends_on "libmikroxml" => :build
-  depends_on "libcssdom"
+  depends_on "libcssom"
   depends_on "libpapki"
   depends_on "libutki"
   depends_on "libr4"
