875112c4c31166f4d8a6d327f129ab7410c427d3
runner <runner@Mac-1617496413727.local>
2021-04-04 00:42:43 +0000

    version 0.1.17 of libcssom.rb

---
+++ libcssom.rb
@@ -0,0 +1,23 @@
+class Libcssom < Formula
+  desc "C++ cross-platform CSS DOM library."
+  homepage "https://github.com/cppfw/cssom"
+  url "https://github.com/cppfw/cssom/archive/0.1.17.tar.gz"
+  sha256 "892516dc2eb68e830acd9d9a1e4caf8a59e7b8ae3171e36ba6eb0180f8c71e10"
+
+  depends_on "prorab" => :build
+  depends_on "prorab-extra" => :build
+  depends_on "myci" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libclargs" => :build
+  depends_on "libpapki"
+  depends_on "libutki"
+
+  def install
+    ENV['PATH'] += ':/usr/local/bin'
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    system "make", "test"
+  end
+end
