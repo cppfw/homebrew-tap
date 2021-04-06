f48fe3df53c0587a69d7e9691f18d045b23fd65f
runner <runner@Mac-1617725893560.local>
2021-04-06 16:23:05 +0000

    version 0.1.83 of myci.rb

--- myci.rb
+++ myci.rb
@@ -1,8 +1,8 @@
 class Myci < Formula
   desc "Helper CI scripts."
   homepage "https://github.com/cppfw/myci"
-  url "https://github.com/cppfw/myci/archive/0.1.82.tar.gz"
-  sha256 "bf17f55782d165b02a2a37e3070ea7b4b5b0ba0f5f598ab760cd71f699e4e453"
+  url "https://github.com/cppfw/myci/archive/0.1.83.tar.gz"
+  sha256 "cbfce7500152ae6eeb7ff7b82386fc319acf6bba060e1aa646a794a9a20e53be"
 
   def install
     ENV['PATH'] += ':/usr/local/bin'
