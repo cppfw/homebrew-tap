1d4f93e1076350d6dfb70dc9fc42d7eab842bcfd
runner <runner@Mac-1617837430991.local>
2021-04-07 23:20:12 +0000

    version 0.1.87 of myci.rb

--- myci.rb
+++ myci.rb
@@ -1,14 +1,16 @@
 class Myci < Formula
   desc "Helper CI scripts."
   homepage "https://github.com/cppfw/myci"
-  url "https://github.com/cppfw/myci/archive/0.1.86.tar.gz"
-  sha256 "0029ccf4d1f5b31e36273556ce38c3a7622bff52a9a19354cfcb78fed6e16dec"
+  url "https://github.com/cppfw/myci/archive/0.1.87.tar.gz"
+  sha256 "891d5ca388007d13c0fd04a48d5148eba7cc23cef74c5fdee2f2f8bc14b8cc96"
+
+  depends_on "md5sha1sum"
 
   def install
     ENV['PATH'] += ':/usr/local/bin'
     system "make", "install", "PREFIX=#{prefix}"
   end
-  
+
   test do
     system "make", "test"
   end
