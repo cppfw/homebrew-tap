0f5116e862fb17951872b1d64eb7c4e1b23f90f8
runner <runner@Mac-1617831611790.local>
2021-04-07 21:51:28 +0000

    version 1.16.3 of prorab.rb

--- prorab.rb
+++ prorab.rb
@@ -1,8 +1,8 @@
 class Prorab < Formula
   desc "GNU make based build system."
   homepage "https://github.com/cppfw/prorab"
-  url "https://github.com/cppfw/prorab/archive/1.16.2.tar.gz"
-  sha256 "2a5c59d7c4b9e15641f5f1976be12a1f500f5a6d96d835d4b3a7471e910600bc"
+  url "https://github.com/cppfw/prorab/archive/1.16.3.tar.gz"
+  sha256 "d47a803d055274d98ecfc9a08104fee86f8e145b71a9a3361a9f8e4a1b155157"
 
   def install
     ENV['PATH'] += ':/usr/local/bin'
