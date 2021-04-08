b25627a21c830c0df66a85bc7718cebeaee2ed18
runner <runner@Mac-1617921583258.local>
2021-04-08 22:45:04 +0000

    version 2.8.3 of libantigrain.rb

--- libantigrain.rb
+++ libantigrain.rb
@@ -1,8 +1,8 @@
 class Libantigrain < Formula
   desc "Anti-grain geometry C++ library."
   homepage "https://github.com/cppfw/agg"
-  url "https://github.com/cppfw/agg/archive/2.8.2.tar.gz"
-  sha256 "1c074cfa922920359dc0bf7b940844e614ed449041c55e42c34422733d71fdc4"
+  url "https://github.com/cppfw/agg/archive/2.8.3.tar.gz"
+  sha256 "bf0be20cd02c0d274a3e5988f8965802197e3f855410bb06dbba6b40672e4d93"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
