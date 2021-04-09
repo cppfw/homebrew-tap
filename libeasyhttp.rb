2e15153f964b40e28065c85028d5771f1bfded29
runner <runner@Mac-1617956812795.local>
2021-04-09 08:43:55 +0000

    version 0.1.7 of libeasyhttp.rb

--- libeasyhttp.rb
+++ libeasyhttp.rb
@@ -1,8 +1,8 @@
 class Libeasyhttp < Formula
   desc "C++ cross-platform HTTP client library."
   homepage "https://github.com/cppfw/easyhttp"
-  url "https://github.com/cppfw/easyhttp/archive/0.1.6.tar.gz"
-  sha256 "944b81a68ecc7c46bceed75a5268bc92ae5140044f5f70b77d8064a67837f797"
+  url "https://github.com/cppfw/easyhttp/archive/0.1.7.tar.gz"
+  sha256 "3dbcdf852624e7a9b9c96468074d196230e25a8b9b915574c5606679ee00426e"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
