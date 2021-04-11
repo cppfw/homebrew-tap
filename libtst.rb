a9c343701b8c6a951638a625050fc452110c52eb
runner <runner@Mac-1618173523518.local>
2021-04-11 22:12:22 +0000

    version 0.1.1 of libtst.rb

---
+++ libtst.rb
@@ -0,0 +1,23 @@
+class Libtst < Formula
+  desc "C++ testing framework."
+  homepage "https://github.com/cppfw/tst"
+  url "https://github.com/cppfw/tst/archive/0.1.1.tar.gz"
+  sha256 "a3a0a2c51386961cbdff83de292b174fc74b71eebe42b6c91ee2f24a6f74a3d9"
+
+  depends_on "prorab" => :build
+  depends_on "prorab-extra" => :build
+  depends_on "myci" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libnitki" => :build
+  depends_on "libutki"
+  depends_on "libclargs"
+
+  def install
+    ENV['PATH'] += ':/usr/local/bin'
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    system "make", "test"
+  end
+end
