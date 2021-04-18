c84adf39e6db379ad3db29607ce60e40571995c9
runner <runner@Mac-1618770304717.local>
2021-04-18 18:41:42 +0000

    version 1.17.5 of prorab.rb

--- prorab.rb
+++ prorab.rb
@@ -1,8 +1,8 @@
 class Prorab < Formula
   desc "GNU make based build system."
   homepage "https://github.com/cppfw/prorab"
-  url "https://github.com/cppfw/prorab/archive/1.17.4.tar.gz"
-  sha256 "d3af027c9612047d6b8fece21c63e9d9d46f1eebc700e4a17c74f61674a00d06"
+  url "https://github.com/cppfw/prorab/archive/1.17.5.tar.gz"
+  sha256 "da7628da8cc7675d91aa9f66577009684192407e565187b0d30c3051a588a64a"
 
   def install
     ENV['PATH'] += ':/usr/local/bin'
