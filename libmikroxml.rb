1fbf453ba826592206a73262daf1464f4788ca4a
runner <runner@Mac-1618861972900.local>
2021-04-19 20:43:51 +0000

    version 0.1.37 of libmikroxml.rb

--- libmikroxml.rb
+++ libmikroxml.rb
@@ -1,8 +1,8 @@
 class Libmikroxml < Formula
   desc "C++ cross-platform XML parser library."
   homepage "https://github.com/cppfw/mikroxml"
-  url "https://github.com/cppfw/mikroxml/archive/0.1.36.tar.gz"
-  sha256 "c70972cdf623c8e48b59855f81c8c4ec86bf33291b74f7a8030bde5b03faa937"
+  url "https://github.com/cppfw/mikroxml/archive/0.1.37.tar.gz"
+  sha256 "e68a32188c55f1f61c17cc49fa519f547d44597c027d6405e407a28a8305f47e"
 
   depends_on "prorab" => :build
   depends_on "pkg-config" => :build
