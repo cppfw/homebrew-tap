13f4f260f2109a8c9929b7b7586b2497d3d703f8
runner <runner@Mac-1619012452988.local>
2021-04-21 13:45:12 +0000

    version 0.1.11 of libtst.rb

--- libtst.rb
+++ libtst.rb
@@ -1,8 +1,8 @@
 class Libtst < Formula
   desc "C++ testing framework."
   homepage "https://github.com/cppfw/tst"
-  url "https://github.com/cppfw/tst/archive/0.1.10.tar.gz"
-  sha256 "a07227f60b5eede279f899e7c68539a5f154fa328ed55e8aa27092f6b850917a"
+  url "https://github.com/cppfw/tst/archive/0.1.11.tar.gz"
+  sha256 "db7b495de822503775e4cb2d3d6ccfedc72eb33af2bcd28ca735f1763ae10221"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
