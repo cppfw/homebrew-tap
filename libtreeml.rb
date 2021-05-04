fade6e2fedaf6451a381bfa44862e1fc79bfecea
runner <runner@Mac-1620154746435.local>
2021-05-04 19:26:04 +0000

    version 0.1.44 of libtreeml.rb

--- libtreeml.rb
+++ libtreeml.rb
@@ -1,8 +1,8 @@
 class Libtreeml < Formula
   desc "C++ cross-platform treeml parser library."
   homepage "https://github.com/cppfw/treeml"
-  url "https://github.com/cppfw/treeml/archive/0.1.43.tar.gz"
-  sha256 "c067d5183fd18fe6c197270b177ff631db729ee6989b6805bcf2e223018cc1e2"
+  url "https://github.com/cppfw/treeml/archive/0.1.44.tar.gz"
+  sha256 "d74224dc92d4d348207a4547acd3ac41642081ef7c45e01455e93bfe075d4f2a"
 
   depends_on "prorab" => :build
   depends_on "libclargs" => :build
