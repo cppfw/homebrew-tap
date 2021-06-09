2d8d36b4adb5a9a45f0b6af4548dcc703abbe7d6
runner <runner@Mac-1623258371537.local>
2021-06-09 18:17:20 +0000

    version 1.0.48 of libnitki.rb

--- libnitki.rb
+++ libnitki.rb
@@ -1,8 +1,8 @@
 class Libnitki < Formula
   desc "C++ cross-platform threading library."
   homepage "https://github.com/cppfw/nitki"
-  url "https://github.com/cppfw/nitki/archive/1.0.47.tar.gz"
-  sha256 "75cab523fe924ea48f395031f6fe9b90aa7ef3202efadfca2cebaaec86becfed"
+  url "https://github.com/cppfw/nitki/archive/1.0.48.tar.gz"
+  sha256 "a7b05778c9f25300b068c66ff498f0fe1691fb128b6fa0962d16132526585da0"
 
   depends_on "prorab" => :build
   depends_on "libopros"
