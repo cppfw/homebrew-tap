ad4a48ebfa407d0854192ede11e5051f2eeb9f40
runner <runner@Mac-1624315319593.local>
2021-06-21 22:52:35 +0000

    version 0.2.69 of mordavokne.rb

--- mordavokne.rb
+++ mordavokne.rb
@@ -1,8 +1,8 @@
 class Mordavokne < Formula
   desc "Intrusive C++ cross-platform OpenGL based GUI library."
   homepage "https://github.com/cppfw/mordavokne"
-  url "https://github.com/cppfw/mordavokne/archive/0.2.68.tar.gz"
-  sha256 "17ebcba5f571c6d7f0a08fcb0ae5bce6e6d70dc83a3056470cf723fd82171af6"
+  url "https://github.com/cppfw/mordavokne/archive/0.2.69.tar.gz"
+  sha256 "a586b0d3fd8506fb297ec242d14a0adb8cbae7000e4f13ffd091aafa733977fc"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
