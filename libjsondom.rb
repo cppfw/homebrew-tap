00a7ae296ca97ce32b772d4dc84ee0cf2ad93804
runner <runner@Mac-1624544071782.local>
2021-06-24 15:00:08 +0000

    version 0.1.13 of libjsondom.rb

--- libjsondom.rb
+++ libjsondom.rb
@@ -1,14 +1,14 @@
 class Libjsondom < Formula
   desc "C++ cross-platform JSON DOM library."
   homepage "https://github.com/cppfw/jsondom"
-  url "https://github.com/cppfw/jsondom/archive/0.1.12.tar.gz"
-  sha256 "0e930b1366983f3eae54fb228beb06354b287d67427df68402fdb5420327b90f"
+  url "https://github.com/cppfw/jsondom/archive/0.1.13.tar.gz"
+  sha256 "e90e2eeb8dae175e6f50ae7d34cb3c2f9244e0d7750d1628fedf1097c34d0944"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
   depends_on "myci" => :build
   depends_on "pkg-config" => :build
-  depends_on "libclargs" => :build
+  depends_on "libtst" => :build
   depends_on "libpapki"
   depends_on "libutki"
 
