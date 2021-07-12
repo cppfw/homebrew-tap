b3e7ce682570a3a508e040243fd4ca813aebfa5c
runner <runner@Mac-1626087453126.local>
2021-07-12 11:00:01 +0000

    version 0.1.103 of myci.rb

--- myci.rb
+++ myci.rb
@@ -1,8 +1,8 @@
 class Myci < Formula
   desc "Helper CI scripts."
   homepage "https://github.com/cppfw/myci"
-  url "https://github.com/cppfw/myci/archive/0.1.102.tar.gz"
-  sha256 "cf8ca7e66387f11757b927cf43fb49b2f8e828c944daa0208bf28446e91be35b"
+  url "https://github.com/cppfw/myci/archive/0.1.103.tar.gz"
+  sha256 "a3ee7bf1c96eda1b1c416b169cadeb787d6e782a7ccb84f42a2b5268b86ebfc7"
 
   depends_on "md5sha1sum"
   depends_on "curl"
