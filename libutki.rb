8bcdaf7ac1f6f4274eaa19a059578c8904b267d1
runner <runner@Mac-1626353238387.local>
2021-07-15 12:52:49 +0000

    version 1.1.154 of libutki.rb

--- libutki.rb
+++ libutki.rb
@@ -1,18 +1,19 @@
 class Libutki < Formula
   desc "C++ utility functions library. Stuff missing from std:: namespace."
   homepage "https://github.com/cppfw/utki"
-  url "https://github.com/cppfw/utki/archive/1.1.153.tar.gz"
-  sha256 "3ee3fa8cc0e1c4ca999e76cb254c6351a534da96759776ae0d8ca6f0b4af4d55"
+  url "https://github.com/cppfw/utki/archive/1.1.154.tar.gz"
+  sha256 "ef1c1e2909af3391f99dab3adf20c48a0ffbbd4125a13fdfceaeb22524951aa8"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
 
   def install
     ENV['PATH'] += ':/usr/local/bin'
-    system "make", "-f", "src/makefile", "install", "PREFIX=#{prefix}"
+    system "make", "--directory", "src", "install", "PREFIX=#{prefix}"
   end
 
   test do
-    system "make", "test"
+	# tests require submodules, so skip those in homebrew recepie
+#    system "make", "test"
   end
 end
