fdc820870bffcf418939b9f7aebd007996336c3a
runner <runner@Mac-1626904594086.local>
2021-07-21 22:09:24 +0000

    version 0.1.48 of libmorda-render-opengl.rb

---
+++ libmorda-render-opengl.rb
@@ -0,0 +1,21 @@
+class LibmordaOpengl2Ren < Formula
+  desc "OpenGl2 renderer for morda GUI library."
+  homepage "https://github.com/cppfw/morda-render-opengl"
+  url "https://github.com/cppfw/morda-render-opengl/archive/0.1.48.tar.gz"
+  sha256 "12a2c21e00699ff9a4c7bb5d84567766959ecdb5725390905ff56aadb2acf0c1"
+
+  depends_on "prorab" => :build
+  depends_on "prorab-extra" => :build
+  depends_on "libmorda"
+  depends_on "glew"
+  depends_on "libr4"
+
+  def install
+    ENV['PATH'] += ':/usr/local/bin'
+    system "make", "-f", "src/makefile", "install", "PREFIX=#{prefix}"
+  end
+
+  #test do
+  #  system "make", "test"
+  #end
+end
