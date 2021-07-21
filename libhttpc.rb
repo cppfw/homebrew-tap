b45b566279afb1ae70a445fa7affc593d19b0e47
runner <runner@Mac-1626877824066.local>
2021-07-21 14:38:15 +0000

    version 0.1.9 of libhttpc.rb

--- libhttpc.rb
+++ libhttpc.rb
@@ -1,8 +1,8 @@
 class Libhttpc < Formula
   desc "C++ cross-platform HTTP client library."
   homepage "https://github.com/cppfw/httpc"
-  url "https://github.com/cppfw/httpc/archive/0.1.8.tar.gz"
-  sha256 "6f88ad3bf1df626944362934cb198c9995b48b98ca9ae27fefc55bedddab9885"
+  url "https://github.com/cppfw/httpc/archive/0.1.9.tar.gz"
+  sha256 "773f682a18e86399a15cdd1ea9ccb4f32973314c2deef9c7a6549cc29b5c6f82"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
