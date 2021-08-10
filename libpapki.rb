3824c59fa2636e69cdf19c595e04ea54619378ae
runner <runner@Mac-1628599546881.local>
2021-08-10 12:50:36 +0000

    version 1.0.108 of libpapki.rb

--- libpapki.rb
+++ libpapki.rb
@@ -1,19 +1,20 @@
 class Libpapki < Formula
   desc "C++ cross-platform file system library."
   homepage "https://github.com/cppfw/papki"
-  url "https://github.com/cppfw/papki/archive/1.0.107.tar.gz"
-  sha256 "24e65a745673a1649872fd62a1de1d9546e144d4dd047e92d8d3ba4ecf9b7961"
+  url "https://github.com/cppfw/papki/archive/1.0.108.tar.gz"
+  sha256 "2d1f011b644a92f3bea3d66623912699520f566fb54efe92b77ff41b8bcf2199"
 
   depends_on "prorab" => :build
   depends_on "libutki"
   depends_on "zlib"
   
+  # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
   def install
     ENV['PATH'] += ':/usr/local/bin'
-    system "make", "--directory", "src", "install", "PREFIX=#{prefix}"
+    system "/usr/local/opt/make/libexec/gnubin/make", "--directory", "src", "install", "PREFIX=#{prefix}"
   end
 
   test do
-    system "make", "test"
+    system "/usr/local/opt/make/libexec/gnubin/make", "test"
   end
 end
