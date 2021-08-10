5153e7974363e3f4b0f37c342fdd977f731c2862
runner <runner@Mac-1628601198621.local>
2021-08-10 13:18:49 +0000

    version 1.0.56 of libopros.rb

--- libopros.rb
+++ libopros.rb
@@ -1,18 +1,19 @@
 class Libopros < Formula
   desc "C++ cross-platform event waiting library."
   homepage "https://github.com/cppfw/opros"
-  url "https://github.com/cppfw/opros/archive/1.0.55.tar.gz"
-  sha256 "13235e047398f513f6a5d7792159471fed062edfedcb66c5c7f39392b1ad228c"
+  url "https://github.com/cppfw/opros/archive/1.0.56.tar.gz"
+  sha256 "5206e22b3e79cf7bce392fa119b737a09ed37012d5fbd2f1c72edc77f92f93dc"
 
   depends_on "prorab" => :build
   depends_on "libutki"
 
+  # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
   def install
     ENV['PATH'] += ':/usr/local/bin'
-    system "make", "-f", "src/makefile", "install", "PREFIX=#{prefix}"
+    system "/usr/local/opt/make/libexec/gnubin/make", "install", "PREFIX=#{prefix}"
   end
 
   test do
-    system "make", "test"
+    system "/usr/local/opt/make/libexec/gnubin/make", "test"
   end
 end
