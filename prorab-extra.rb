34438e55e5bc92b39046b043b2b8f3eb4ae46df8
runner <runner@Mac-1628594292228.local>
2021-08-10 11:20:40 +0000

    version 0.2.30 of prorab-extra.rb

--- prorab-extra.rb
+++ prorab-extra.rb
@@ -1,17 +1,19 @@
 class ProrabExtra < Formula
   desc "GNU make based build system."
   homepage "https://github.com/cppfw/prorab-extra"
-  url "https://github.com/cppfw/prorab-extra/archive/0.2.29.tar.gz"
-  sha256 "2051d432d664c47f7debc67e3ea0c31896bdc67ef24d9beb050a766d96c23b04"
+  url "https://github.com/cppfw/prorab-extra/archive/0.2.30.tar.gz"
+  sha256 "783c9ece38fc3c9c2a7317edd7664969d3e9f00a99df145bfa37bf4222eb2b5f"
 
+  depends_on "make"
   depends_on "prorab"
 
+  # use gmake here because otherwise homebrew uses default Mac's make which is of old version 3.81 which has bugs
   def install
     ENV['PATH'] += ':/usr/local/bin'
-    system "make", "install", "PREFIX=#{prefix}"
+    system "/usr/local/opt/make/libexec/gnubin/make", "install", "PREFIX=#{prefix}"
   end
   
   test do
-    system "make", "test"
+    system "/usr/local/opt/make/libexec/gnubin/make", "test"
   end
 end
