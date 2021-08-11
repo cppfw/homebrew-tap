e42f99dca350996f5c202c35df6bbd0a4bd5afa4
runner <runner@Mac-1628681399966.local>
2021-08-11 11:45:55 +0000

    version 1.0.23 of libsetka.rb

--- libsetka.rb
+++ libsetka.rb
@@ -1,8 +1,8 @@
 class Libsetka < Formula
   desc "C++ cross-platform network library."
   homepage "https://github.com/cppfw/setka"
-  url "https://github.com/cppfw/setka/archive/1.0.22.tar.gz"
-  sha256 "96ebd17148811070554f01641b3a9465f8e2d12f8b54abda8cbc23ae22bfa13e"
+  url "https://github.com/cppfw/setka/archive/1.0.23.tar.gz"
+  sha256 "970de4f4b4cb4a0690b71dfb9589f2e5c1ce9eb6093bb142167c70dbb01b2509"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
@@ -12,13 +12,14 @@ class Libsetka < Formula
   depends_on "libutki"
   depends_on "libpapki"
 
+  # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
   def install
     ENV['PATH'] += ':/usr/local/bin'
-    system "make", "install", "PREFIX=#{prefix}"
+    system "/usr/local/opt/make/libexec/gnubin/make", "install", "PREFIX=#{prefix}"
   end
 
   # TODO: uncomment
   #test do
-  #  system "make", "test"
+  #  system "/usr/local/opt/make/libexec/gnubin/make", "test"
   #end
 end
