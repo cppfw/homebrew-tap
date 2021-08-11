2d45167e39ee531db63c9f9a66b5f52446bf8a5a
runner <runner@Mac-1628677802096.local>
2021-08-11 10:33:51 +0000

    version 0.3.6 of libtst.rb

--- libtst.rb
+++ libtst.rb
@@ -1,8 +1,8 @@
 class Libtst < Formula
   desc "C++ testing framework."
   homepage "https://github.com/cppfw/tst"
-  url "https://github.com/cppfw/tst/archive/0.3.5.tar.gz"
-  sha256 "20fd1166bdeb741e0387aca5706bf3981ec747a32576332c52717513f42f2d4d"
+  url "https://github.com/cppfw/tst/archive/0.3.6.tar.gz"
+  sha256 "b58b685c0072829267e1e9f263a74de1bdf92893387087077daab343be39b3d4"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
@@ -12,12 +12,13 @@ class Libtst < Formula
   depends_on "libutki"
   depends_on "libclargs"
 
+  # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
   def install
     ENV['PATH'] += ':/usr/local/bin'
-    system "make", "install", "PREFIX=#{prefix}"
+    system "/usr/local/opt/make/libexec/gnubin/make", "install", "PREFIX=#{prefix}"
   end
 
   test do
-    system "make", "test"
+    system "/usr/local/opt/make/libexec/gnubin/make", "test"
   end
 end
