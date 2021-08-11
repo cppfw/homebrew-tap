603ae2a056366093ffdc43bba21155b412770d76
runner <runner@Mac-1628681069079.local>
2021-08-11 11:32:09 +0000

    version 0.3.35 of libaudout.rb

--- libaudout.rb
+++ libaudout.rb
@@ -1,19 +1,20 @@
 class Libaudout < Formula
   desc "C++ cross-platform audio output library."
   homepage "https://github.com/cppfw/audout"
-  url "https://github.com/cppfw/audout/archive/0.3.34.tar.gz"
-  sha256 "bd65ae029d9c694817537b1e7abb6b9bf51f229d031abc5dd5fac30623f287e8"
+  url "https://github.com/cppfw/audout/archive/0.3.35.tar.gz"
+  sha256 "458726911c55f6da83135acdcc2cd83d46f168cef411574679db8dbf7ebe692a"
 
   depends_on "prorab" => :build
   depends_on "libnitki" => :build
   depends_on "libutki"
-  
+
+  # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81  
   def install
     ENV['PATH'] += ':/usr/local/bin'
-    system "make", "-C", "src", "install", "PREFIX=#{prefix}"
+    system "/usr/local/opt/make/libexec/gnubin/make", "install", "PREFIX=#{prefix}"
   end
 
   test do
-    system "make", "test"
+    system "/usr/local/opt/make/libexec/gnubin/make", "test"
   end
 end
