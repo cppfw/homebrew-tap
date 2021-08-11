83cca36383e03c0db8c3b8580f5fd14f57a76873
runner <runner@Mac-1628683479571.local>
2021-08-11 12:13:21 +0000

    version 1.0.58 of libr4.rb

--- libr4.rb
+++ libr4.rb
@@ -1,19 +1,20 @@
 class Libr4 < Formula
   desc "C++ 3d math library."
   homepage "https://github.com/cppfw/r4"
-  url "https://github.com/cppfw/r4/archive/1.0.57.tar.gz"
-  sha256 "51c000010c492c88e6ba9c649c1ff56263ccc521298c88fd448158128cbf7d72"
+  url "https://github.com/cppfw/r4/archive/1.0.58.tar.gz"
+  sha256 "117d1edb647f0bb2f122d3c3a424660f829a2e59e64f929ee10c89bc154be56d"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
   depends_on "libutki"
 
+  # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
   def install
     ENV['PATH'] += ':/usr/local/bin'
-    system "make", "-f", "src/makefile", "install", "PREFIX=#{prefix}"
+    system "/usr/local/opt/make/libexec/gnubin/make", "install", "PREFIX=#{prefix}"
   end
 
   test do
-    system "make", "test"
+    system "/usr/local/opt/make/libexec/gnubin/make", "test"
   end
 end
