837bdc1df56967acdc8bda90fc82beeaf9dd6b89
runner <runner@Mac-1628683756694.local>
2021-08-11 12:20:21 +0000

    version 0.1.44 of libmikroxml.rb

--- libmikroxml.rb
+++ libmikroxml.rb
@@ -1,8 +1,8 @@
 class Libmikroxml < Formula
   desc "C++ cross-platform XML parser library."
   homepage "https://github.com/cppfw/mikroxml"
-  url "https://github.com/cppfw/mikroxml/archive/0.1.43.tar.gz"
-  sha256 "20c9d7f55c1dfe0a9e4cbf3c5902c277f31d2188410f8ee5c3ba0661759a2c4c"
+  url "https://github.com/cppfw/mikroxml/archive/0.1.44.tar.gz"
+  sha256 "99d17a56cfb0e46d18e518bc08956cb91bf94e4626d523d010b168fe259255c6"
 
   depends_on "prorab" => :build
   depends_on "pkg-config" => :build
@@ -10,12 +10,13 @@ class Libmikroxml < Formula
   depends_on "libpapki" => :build # needed for tests
   depends_on "libutki"
 
+  # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
   def install
     ENV['PATH'] += ':/usr/local/bin'
-    system "make", "-f", "src/makefile", "install", "PREFIX=#{prefix}"
+    system "/usr/local/opt/make/libexec/gnubin/make", "install", "PREFIX=#{prefix}"
   end
 
   test do
-    system "make", "test"
+    system "/usr/local/opt/make/libexec/gnubin/make", "test"
   end
 end
