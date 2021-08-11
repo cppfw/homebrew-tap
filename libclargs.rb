316a09c461980bfce1ad732619959e9138f1a612
runner <runner@Mac-1628677065950.local>
2021-08-11 10:23:55 +0000

    version 0.2.32 of libclargs.rb

--- libclargs.rb
+++ libclargs.rb
@@ -1,20 +1,21 @@
 class Libclargs < Formula
   desc "C++ cross-platform library for parsing command line arguments."
   homepage "https://github.com/cppfw/clargs"
-  url "https://github.com/cppfw/clargs/archive/0.2.30.tar.gz"
-  sha256 "2359a8703620fa7743b5c85f0ff00f9d32b7f8ec8d9d919f0aef7156e85d0678"
+  url "https://github.com/cppfw/clargs/archive/0.2.32.tar.gz"
+  sha256 "abf905f82e3232403e9f1bcf401ade8ae8986efc0ccce1fd4a7bb58810236865"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
   depends_on "myci" => :build
   depends_on "libutki"
 
+  # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
   def install
     ENV['PATH'] += ':/usr/local/bin'
-    system "make", "install", "PREFIX=#{prefix}"
+    system "/usr/local/opt/make/libexec/gnubin/make", "install", "PREFIX=#{prefix}"
   end
 
   test do
-    system "make", "test"
+    system "/usr/local/opt/make/libexec/gnubin/make", "test"
   end
 end
