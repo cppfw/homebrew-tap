a80d90c7939502adf1b898a6e98cc3f4b60a8649
runner <runner@Mac-1628767623005.local>
2021-08-12 11:37:31 +0000

    version 0.5.19 of libsvgren.rb

--- libsvgren.rb
+++ libsvgren.rb
@@ -1,8 +1,8 @@
 class Libsvgren < Formula
   desc "C++ cross-platform SVG rendering library."
   homepage "https://github.com/igagis/svgren"
-  url "https://github.com/igagis/svgren/archive/0.5.18.tar.gz"
-  sha256 "7bff233d820fdda05c72e4186dd1db42ae2714206effb1248143033d98f4e782"
+  url "https://github.com/igagis/svgren/archive/0.5.19.tar.gz"
+  sha256 "bcab53542ea0856a4a7a5d73dd2ed70109de69055d095a5049b8b031b56c0a1c"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
@@ -15,12 +15,13 @@ class Libsvgren < Formula
   depends_on "libsvgdom"
   depends_on "libutki"
 
+  # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
   def install
     ENV['PATH'] += ':/usr/local/bin'
-    system "make", "install", "PREFIX=#{prefix}"
+    system "/usr/local/opt/make/libexec/gnubin/make", "install", "PREFIX=#{prefix}"
   end
 
   test do
-    system "make", "test"
+    system "/usr/local/opt/make/libexec/gnubin/make", "test"
   end
 end
