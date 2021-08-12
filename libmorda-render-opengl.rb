05126574f3562465069060dd56d941c2be6e7aae
runner <runner@Mac-1628776489002.local>
2021-08-12 14:04:30 +0000

    version 0.1.53 of libmorda-render-opengl.rb

--- libmorda-render-opengl.rb
+++ libmorda-render-opengl.rb
@@ -1,8 +1,8 @@
 class LibmordaRenderOpengl < Formula
   desc "OpenGl renderer for morda GUI library."
   homepage "https://github.com/cppfw/morda-render-opengl"
-  url "https://github.com/cppfw/morda-render-opengl/archive/0.1.52.tar.gz"
-  sha256 "3b5b6b92be7a5beb1594cfd2559b0879daeef6b33d296d7c8a9fb7662983c5d8"
+  url "https://github.com/cppfw/morda-render-opengl/archive/0.1.53.tar.gz"
+  sha256 "d68889397fecefd391ee91e1a7935a360c3e66dafe7ee64f742d5f3e0e5287f6"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
@@ -10,12 +10,13 @@ class LibmordaRenderOpengl < Formula
   depends_on "glew"
   depends_on "libr4"
 
+  # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
   def install
     ENV['PATH'] += ':/usr/local/bin'
-    system "make", "-f", "src/makefile", "install", "PREFIX=#{prefix}"
+    system "/usr/local/opt/make/libexec/gnubin/make", "install", "PREFIX=#{prefix}"
   end
 
-  #test do
-  #  system "make", "test"
-  #end
+  test do
+    system "/usr/local/opt/make/libexec/gnubin/make", "test"
+  end
 end
