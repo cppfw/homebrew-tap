a8a4ca6e1eb02123fe562ec3ce92db53de4ccc5d
runner <runner@Mac-1630272374401.local>
2021-08-29 21:36:31 +0000

    version 1.0.60 of libr4.rb

--- libr4.rb
+++ libr4.rb
@@ -1,8 +1,8 @@
 class Libr4 < Formula
   desc "C++ 3d math library."
   homepage "https://github.com/cppfw/r4"
-  url "https://github.com/cppfw/r4/archive/1.0.59.tar.gz"
-  sha256 "1069c9a4fb9349d8955e09ebeebd416f07b55ec677b576d6b148acf09a2762b1"
+  url "https://github.com/cppfw/r4/archive/1.0.60.tar.gz"
+  sha256 "052ef31fd715a4d38dedbd66802dd41d61e9aea40d12e516048191ac0e863ad2"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
