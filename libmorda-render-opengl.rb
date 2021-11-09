9dd81f766b214ae4f958d4292db4d919f442d7de
runner <runner@Mac-1636456591269.local>
2021-11-09 12:10:37 +0000

    version 0.1.55 of libmorda-render-opengl.rb

--- libmorda-render-opengl.rb
+++ libmorda-render-opengl.rb
@@ -1,8 +1,8 @@
 class LibmordaRenderOpengl < Formula
   desc "OpenGl renderer for morda GUI library."
   homepage "https://github.com/cppfw/morda-render-opengl"
-  url "https://github.com/cppfw/morda-render-opengl/archive/0.1.54.tar.gz"
-  sha256 "a432b01be31e6f4117b0b0d01ef031b45666118da94677d29fd6109c3ed521ec"
+  url "https://github.com/cppfw/morda-render-opengl/archive/0.1.55.tar.gz"
+  sha256 "8f400ed1277dce95705a6338d5a9a20f92e6e441f97e2ca04d27de7c309ff7cc"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
