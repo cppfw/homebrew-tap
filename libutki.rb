c527197aadfc8113033bf5a18b8f7df335945bca
runner <runner@Mac-1665583435908.local>
2022-10-12 14:17:40 +0000

    version 1.1.173 of libutki.rb

--- libutki.rb
+++ libutki.rb
@@ -1,11 +1,12 @@
 class Libutki < Formula
   desc "C++ utility functions library. Stuff missing from std:: namespace."
   homepage "https://github.com/cppfw/utki"
-  url "https://github.com/cppfw/utki/archive/1.1.172.tar.gz"
-  sha256 "441422058df5d9237d4e96385a9f617e8c551bd6d7f532e6574f92dc6b743069"
+  url "https://github.com/cppfw/utki/archive/1.1.173.tar.gz"
+  sha256 "f61a24185485c6f6d36a804373e458f59f342f2ba52ec7557f908c695b86d8e2"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
+  depends_on "clang-format" => :build
 
   # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
   def install
