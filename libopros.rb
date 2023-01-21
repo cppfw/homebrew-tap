fd360d0383cfd72e544fd5fe84ece4973db309da
runner <runner@Mac-1674318307303.local>
2023-01-21 19:32:47 +0000

    version 1.0.69 of libopros.rb

--- libopros.rb
+++ libopros.rb
@@ -1,8 +1,8 @@
 class Libopros < Formula
   desc "C++ cross-platform event waiting library."
   homepage "https://github.com/cppfw/opros"
-  url "https://github.com/cppfw/opros/archive/1.0.68.tar.gz"
-  sha256 "c932b55331f7f5a880c24d26748d0dcb85850dcac0bf086aef18111ffd44b038"
+  url "https://github.com/cppfw/opros/archive/1.0.69.tar.gz"
+  sha256 "f5b2cb251165ef243ef34a736cde265d56e0fb7e46383d8a2c6b9f025ab97f21"
 
   depends_on "prorab" => :build
   depends_on "libutki"
