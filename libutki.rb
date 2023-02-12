ba0d29e33cdd5a802fc51b06d9885e9b5e081036
runner <runner@Mac-1676225973631.local>
2023-02-12 18:50:00 +0000

    version 1.1.186 of libutki.rb

--- libutki.rb
+++ libutki.rb
@@ -1,8 +1,8 @@
 class Libutki < Formula
   desc "C++ utility functions library. Stuff missing from std:: namespace."
   homepage "https://github.com/cppfw/utki"
-  url "https://github.com/cppfw/utki/archive/1.1.185.tar.gz"
-  sha256 "7faab13ee153ef851f97ede00a095e45a68ac72f4954dc045353f99564c1cca8"
+  url "https://github.com/cppfw/utki/archive/1.1.186.tar.gz"
+  sha256 "767442df5ed0b8ede0ffe4531f2d8a8cb5bfedf53c7fcf1d745129f5659da77f"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
@@ -11,7 +11,7 @@ class Libutki < Formula
   def install
     ENV['PATH'] += ':/usr/local/bin'
     # install from src directory because tests require submodules
-    system "/usr/local/opt/make/libexec/gnubin/make", "--directory", "src", "install", "PREFIX=#{prefix}"
+    system "/usr/local/opt/make/libexec/gnubin/make", "--directory", "src", "install", "PREFIX=#{prefix}", "lint=off"
   end
 
   test do
