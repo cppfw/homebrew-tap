0848d2d06a62eb9565f988b031e7b610e9ec8c05
runner <runner@Mac-1676559020057.local>
2023-02-16 14:57:56 +0000

    version 0.1.54 of libmikroxml.rb

--- libmikroxml.rb
+++ libmikroxml.rb
@@ -1,8 +1,8 @@
 class Libmikroxml < Formula
   desc "C++ cross-platform XML parser library."
   homepage "https://github.com/cppfw/mikroxml"
-  url "https://github.com/cppfw/mikroxml/archive/0.1.53.tar.gz"
-  sha256 "8b7bce82328db531dbce71b99ad8d056737ccdcf4f75ea2168274939725e113a"
+  url "https://github.com/cppfw/mikroxml/archive/0.1.54.tar.gz"
+  sha256 "54e6d395dae7c1a4c315c2072f34a349afc7b1eceee69729798f2de225bead4e"
 
   depends_on "prorab" => :build
   depends_on "pkg-config" => :build
@@ -13,7 +13,7 @@ class Libmikroxml < Formula
   # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
   def install
     ENV['PATH'] += ':/usr/local/bin'
-    system "/usr/local/opt/make/libexec/gnubin/make", "install", "PREFIX=#{prefix}"
+    system "/usr/local/opt/make/libexec/gnubin/make", "install", "PREFIX=#{prefix}", "lint=off"
   end
 
   test do
