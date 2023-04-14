c74e30b69f7db5132cb5689a28632294f12fbea1
runner <runner@Mac-1681470991309.local>
2023-04-14 11:23:54 +0000

    version 0.1.1 of liburlmodel.rb

---
+++ liburlmodel.rb
@@ -0,0 +1,22 @@
+class Liburlmodel < Formula
+  desc "URL model C++ library."
+  homepage "https://github.com/cppfw/urlmodel"
+  url "https://github.com/cppfw/urlmodel/archive/0.1.1.tar.gz"
+  sha256 "6c25fd394f2905a1ed566e97b057f3b60b5e4954165906f8b03d5dbd1fe452df"
+
+  depends_on "prorab" => :build
+  depends_on "prorab-extra" => :build
+  depends_on "libtst" => :build
+  depends_on "libutki"
+
+  # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
+  def install
+    ENV['PATH'] += ':/usr/local/bin'
+    # install from src directory because tests require submodules
+    system "/usr/local/opt/make/libexec/gnubin/make", "--directory", "src", "install", "PREFIX=#{prefix}", "lint=off"
+  end
+
+  test do
+    system "/usr/local/opt/make/libexec/gnubin/make", "test"
+  end
+end
