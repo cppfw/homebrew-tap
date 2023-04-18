2ed470d41f4339859ace6789375dcc9a6a1f3839
runner <runner@Mac-1681845842647.local>
2023-04-18 19:28:58 +0000

    version 0.1.4 of libhttpmodel.rb

--- libhttpmodel.rb
+++ libhttpmodel.rb
@@ -1,8 +1,8 @@
 class Libhttpmodel < Formula
   desc "HTTP model C++ library."
   homepage "https://github.com/cppfw/httpmodel"
-  url "https://github.com/cppfw/httpmodel/archive/0.1.3.tar.gz"
-  sha256 "ff998ca044d12dddde44c6884676ea1bb19d638bfec94a98e7c503f7b277bb7b"
+  url "https://github.com/cppfw/httpmodel/archive/0.1.4.tar.gz"
+  sha256 "39b7b2bbc68282a1d88e79fef21c98f8683a2e0b7b623814a81187a91f881057"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
