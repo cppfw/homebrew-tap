88c7f711bb2d4a49c1416075909c361d7b46b341
runner <runner@Mac-1683222772478.local>
2023-05-04 18:08:04 +0000

    version 0.1.4 of liburlmodel.rb

--- liburlmodel.rb
+++ liburlmodel.rb
@@ -1,8 +1,8 @@
 class Liburlmodel < Formula
   desc "URL model C++ library."
   homepage "https://github.com/cppfw/urlmodel"
-  url "https://github.com/cppfw/urlmodel/archive/0.1.3.tar.gz"
-  sha256 "6307d85328eea53eea06b6beafb55e2cf11cc8b4f011dfbae8d913347504d0e6"
+  url "https://github.com/cppfw/urlmodel/archive/0.1.4.tar.gz"
+  sha256 "11fe3b83ef13abd89457ed8cd11ea3249d393ebf025509bb86fba06e8dd58cec"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
