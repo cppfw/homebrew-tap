efc6971d6384234ede83bff391433f49c36f491a
runner <runner@Mac-1687546547476.local>
2023-06-23 20:18:22 +0000

    version 0.1.1 of librasterimage.rb

---
+++ librasterimage.rb
@@ -0,0 +1,26 @@
+class Librasterimage < Formula
+  desc "C++ cross-platform SVG rendering library."
+  homepage "https://github.com/igagis/rasterimage"
+  url "https://github.com/igagis/rasterimage/archive/0.1.1.tar.gz"
+  sha256 "cef08c11262ff86069e2b64b6630fa521e40bd3d6d43d870fd73f49f92f044bd"
+
+  depends_on "prorab" => :build
+  depends_on "prorab-extra" => :build
+  depends_on "myci" => :build
+  depends_on "libtst" => :build
+  depends_on "libpng"
+  depends_on "jpeg"
+  depends_on "libutki"
+  depends_on "libpapki"
+  depends_on "libr4"
+
+  # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
+  def install
+    ENV['PATH'] += ':/usr/local/bin'
+    system "/usr/local/opt/make/libexec/gnubin/make", "install", "PREFIX=#{prefix}", "lint=off"
+  end
+
+  test do
+    system "/usr/local/opt/make/libexec/gnubin/make", "test"
+  end
+end
