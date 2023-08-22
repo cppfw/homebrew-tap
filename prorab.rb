4373151f3c4412102e9d3ce59e0596f04c483508
runner <runner@Mac-1692715296037.local>
2023-08-22 14:44:48 +0000

    version 2.0.18 of prorab.rb

--- prorab.rb
+++ prorab.rb
@@ -1,8 +1,8 @@
 class Prorab < Formula
   desc "GNU make based build system."
   homepage "https://github.com/cppfw/prorab"
-  url "https://github.com/cppfw/prorab/archive/2.0.17.tar.gz"
-  sha256 "5f01aad90b20173e6938de214b50de07f51ccf2736cd860fbef4398600b5ee10"
+  url "https://github.com/cppfw/prorab/archive/2.0.18.tar.gz"
+  sha256 "1220b6bcac601ed048a2e9d903affa9594bd6881ab28ec484fa37254312044d9"
 
   depends_on "dos2unix" => :build
   depends_on "make"
