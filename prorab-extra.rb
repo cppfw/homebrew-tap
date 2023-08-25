84e48067e3f88a240b00077873f3f4563f2bb78e
runner <runner@Mac-1692997029799.local>
2023-08-25 21:07:42 +0000

    version 0.2.47 of prorab-extra.rb

--- prorab-extra.rb
+++ prorab-extra.rb
@@ -1,8 +1,8 @@
 class ProrabExtra < Formula
   desc "GNU make based build system."
   homepage "https://github.com/cppfw/prorab-extra"
-  url "https://github.com/cppfw/prorab-extra/archive/0.2.46.tar.gz"
-  sha256 "835ecbbe8b0486cc82d2dffd2b96dbc6d6531120db5f4b546be6a0625d0b9d52"
+  url "https://github.com/cppfw/prorab-extra/archive/0.2.47.tar.gz"
+  sha256 "fb154406c669c0dd26a7e2d29e3de79ee0f5ee1fb935802fa30e49aeedfc47e5"
 
   depends_on "make"
   depends_on "myci"
