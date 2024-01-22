114ffdab217fbe269af5592e8e3ea6075168aba1
runner <runner@Mac-1705926598101.local>
2024-01-22 12:43:05 +0000

    version 0.5.210 of libruis.rb

---
+++ libruis.rb
@@ -0,0 +1,32 @@
+class Libruis < Formula
+  desc "C++ cross-platform OpenGL based GUI library."
+  homepage "https://github.com/cppfw/ruis"
+  url "https://github.com/cppfw/ruis/archive/0.5.210.tar.gz"
+  sha256 "d02a4364e6ca990f9b722fbf4dd86b98d178255d17bd45e91fffa5aba435f6e2"
+
+  depends_on "prorab" => :build
+  depends_on "prorab-extra" => :build
+  depends_on "myci" => :build
+  depends_on "glew" => :build
+  depends_on "libnitki" => :build
+  depends_on "libtst" => :build
+  depends_on "libutki"
+  depends_on "libpapki"
+  depends_on "libtreeml"
+  depends_on "librasterimage"
+  depends_on "z"
+  depends_on "freetype"
+  depends_on "libr4"
+  depends_on "libsvgdom"
+  depends_on "libsvgren"
+  
+  # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
+  def install
+    ENV['PATH'] += ':/usr/local/bin'
+    system "/usr/local/opt/make/libexec/gnubin/make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    system "/usr/local/opt/make/libexec/gnubin/make", "test"
+  end
+end
