cda000cd45e231bcbc5fa94ce5a945f9632cf398
runner <runner@Mac-1705934212587.local>
2024-01-22 14:48:41 +0000

    version 0.1.63 of libruis-render-opengl.rb

---
+++ libruis-render-opengl.rb
@@ -0,0 +1,22 @@
+class LibRuisRenderOpengl < Formula
+  desc "OpenGl renderer for ruis GUI library."
+  homepage "https://github.com/cppfw/ruis-render-opengl"
+  url "https://github.com/cppfw/ruis-render-opengl/archive/0.1.63.tar.gz"
+  sha256 "befd4b98c84160e579e1f545f37a97104a2e6f34fcd972782187fcbee6dcb516"
+
+  depends_on "prorab" => :build
+  depends_on "prorab-extra" => :build
+  depends_on "libruis"
+  depends_on "glew"
+  depends_on "libr4"
+
+  # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
+  def install
+    ENV['PATH'] += ':/usr/local/bin'
+    system "/usr/local/opt/make/libexec/gnubin/make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    system "/usr/local/opt/make/libexec/gnubin/make", "test"
+  end
+end
