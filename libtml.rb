e2ac6a443bdf9503096df15db6f1dfd568733cd2
runner <runner@Mac-1709155794827.local>
2024-02-28 21:45:50 +0000

    version 0.2.17 of libtml.rb

--- libtml.rb
+++ libtml.rb
@@ -1,8 +1,8 @@
 class Libtml < Formula
   desc "C++ cross-platform tml parser library."
   homepage "https://github.com/cppfw/tml"
-  url "https://github.com/cppfw/tml/archive/0.2.16.tar.gz"
-  sha256 "c8cc459b12d8336155946879abb9c08946365e9977ec1582e6b1f592bd7d1184"
+  url "https://github.com/cppfw/tml/archive/0.2.17.tar.gz"
+  sha256 "a244d64b9a0204f4ef988f1699d9ce3286e2d6fcc30763afbaa78b34232d20ae"
 
   depends_on "prorab" => :build
   depends_on "libclargs" => :build
