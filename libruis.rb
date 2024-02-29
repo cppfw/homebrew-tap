39e50f8d205dea4e741dd836fc0306ed4f6e358f
runner <runner@Mac-1709247155048.local>
2024-02-29 23:23:18 +0000

    version 0.5.215 of libruis.rb

--- libruis.rb
+++ libruis.rb
@@ -1,8 +1,8 @@
 class Libruis < Formula
   desc "C++ cross-platform OpenGL based GUI library."
   homepage "https://github.com/cppfw/ruis"
-  url "https://github.com/cppfw/ruis/archive/0.5.214.tar.gz"
-  sha256 "b7a39e7a744316c0bd9ed426ed651d1f3904f579557a3f0809e73d5b880f6980"
+  url "https://github.com/cppfw/ruis/archive/0.5.215.tar.gz"
+  sha256 "f2a81c7caa38dac832fcad597d539e0bf56d4edb86a55482460c336912993d00"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
@@ -12,7 +12,7 @@ class Libruis < Formula
   depends_on "libtst" => :build
   depends_on "libutki"
   depends_on "libpapki"
-  depends_on "libtreeml"
+  depends_on "libtml"
   depends_on "librasterimage"
   depends_on "z"
   depends_on "freetype"
