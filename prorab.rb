cb5b86a036b843d2aef9cf375c877ef799da594a
Anka <runner@Mac-1714241776171.local>
2024-04-27 18:38:03 +0000

    version 2.0.23 of prorab.rb

--- prorab.rb
+++ prorab.rb
@@ -1,8 +1,8 @@
 class Prorab < Formula
   desc "GNU make based build system."
   homepage "https://github.com/cppfw/prorab"
-  url "https://github.com/cppfw/prorab/archive/2.0.22.tar.gz"
-  sha256 "5c5097204dec2ec4af9aac4389c17b7c5ea65ee368c8813059546300b16a8f54"
+  url "https://github.com/cppfw/prorab/archive/2.0.23.tar.gz"
+  sha256 "c10678e098c36ebf21b5ca1858a9e5c0b507541628bb061e54d9be29ad3204c4"
 
   depends_on "dos2unix" => :build
   depends_on "make"
@@ -10,10 +10,10 @@ class Prorab < Formula
   # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
   def install
     ENV['PATH'] += ':/usr/local/bin'
-    system "/usr/local/opt/make/libexec/gnubin/make", "install", "PREFIX=#{prefix}"
+    system "gmake", "install", "PREFIX=#{prefix}"
   end
   
   test do
-    system "/usr/local/opt/make/libexec/gnubin/make", "test"
+    system "gmake", "test"
   end
 end
