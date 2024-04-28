fcb7c14a4376c7d393316fb1da4e1a59be949c13
Anka <runner@Mac-1714324838405.local>
2024-04-28 18:07:39 +0000

    version 1.1.238 of libutki.rb

--- libutki.rb
+++ libutki.rb
@@ -1,8 +1,8 @@
 class Libutki < Formula
   desc "C++ utility functions library. Stuff missing from std:: namespace."
   homepage "https://github.com/cppfw/utki"
-  url "https://github.com/cppfw/utki/archive/1.1.237.tar.gz"
-  sha256 "8b60395e3b475f1644261d7c816f1e988e39f1fadd97536a25cf172262692b48"
+  url "https://github.com/cppfw/utki/archive/1.1.238.tar.gz"
+  sha256 "af11f8ae042ff9b6bf17216319a25292b3e9e2858812e4f7292bcde9f381bf4e"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
@@ -10,13 +10,12 @@ class Libutki < Formula
   # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
   def install
     ENV['PATH'] += ':/usr/local/bin'
-    system "gmake", "--version"
     # install from src directory because tests require submodules
-    system "gmake", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "--directory=src", "install", "PREFIX=#{prefix}", "lint=off"
+    system "#{ENV['HOMEBREW_PREFIX']}/opt/make/libexec/gnubin/make", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "--directory=src", "install", "PREFIX=#{prefix}", "lint=off"
   end
 
   test do
 	# tests require submodules, so skip those in homebrew recepie
-#    system "gmake", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "test"
+#    system "#{ENV['HOMEBREW_PREFIX']}/opt/make/libexec/gnubin/make", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "test"
   end
 end
