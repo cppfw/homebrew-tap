f94c631fa854d54035954b2e0c16c701970bfd4b
Anka <runner@Mac-1714311840666.local>
2024-04-28 14:20:56 +0000

    version 0.2.51 of prorab-extra.rb

--- prorab-extra.rb
+++ prorab-extra.rb
@@ -1,8 +1,8 @@
 class ProrabExtra < Formula
   desc "GNU make based build system."
   homepage "https://github.com/cppfw/prorab-extra"
-  url "https://github.com/cppfw/prorab-extra/archive/0.2.50.tar.gz"
-  sha256 "beb7efef644051fbb8a739e1126d2e5e4d5c7058f0e646218d3c9c11bf8ce93f"
+  url "https://github.com/cppfw/prorab-extra/archive/0.2.51.tar.gz"
+  sha256 "8f027b3d03a440a1ec999dbdce6259989d9cd42f2ee536c425986eece940f447"
 
   depends_on "make"
   depends_on "myci"
@@ -11,10 +11,10 @@ class ProrabExtra < Formula
   # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
   def install
     ENV['PATH'] += ':/usr/local/bin'
-    system "gmake", "--include-dir=$(brew --prefix)/include", "install", "PREFIX=#{prefix}"
+    system "gmake", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "install", "PREFIX=#{prefix}"
   end
   
   test do
-    system "gmake", "--include-dir=$(brew --prefix)/include", "test"
+    system "gmake", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "test"
   end
 end
