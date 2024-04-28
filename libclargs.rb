8071c76fcb07c79090784a4287c3f039c0100340
Anka <runner@Mac-1714328178665.local>
2024-04-28 18:52:29 +0000

    version 0.2.61 of libclargs.rb

--- libclargs.rb
+++ libclargs.rb
@@ -1,8 +1,8 @@
 class Libclargs < Formula
   desc "C++ cross-platform library for parsing command line arguments."
   homepage "https://github.com/cppfw/clargs"
-  url "https://github.com/cppfw/clargs/archive/0.2.59.tar.gz"
-  sha256 "9bc1119068463752668642084759f4e3c3ff6e5372c51ebf95f50e99d16da838"
+  url "https://github.com/cppfw/clargs/archive/0.2.61.tar.gz"
+  sha256 "9be03eda3c7be7c761bcc8f17604712bf15f89ef73864b5146b6e0ebb84a1753"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
@@ -12,11 +12,11 @@ class Libclargs < Formula
   # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
   def install
     ENV['PATH'] += ':/usr/local/bin'
-    system "/usr/local/opt/make/libexec/gnubin/make", "--directory", "src", "install", "PREFIX=#{prefix}"
+    system "#{ENV['HOMEBREW_PREFIX']}/opt/make/libexec/gnubin/make", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "--directory=src", "install", "PREFIX=#{prefix}", "lint=off"
   end
 
   test do
     # tests require submodules, so skip those in homebrew recepie
-#    system "/usr/local/opt/make/libexec/gnubin/make", "test"
+#    system "#{ENV['HOMEBREW_PREFIX']}/opt/make/libexec/gnubin/make", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "test"
   end
 end
