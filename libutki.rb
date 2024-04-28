1f9f4773f4c152c7df12ab863af305510259aacd
Anka <runner@Mac-1714317410454.local>
2024-04-28 15:57:25 +0000

    version 1.1.236 of libutki.rb

--- libutki.rb
+++ libutki.rb
@@ -1,8 +1,8 @@
 class Libutki < Formula
   desc "C++ utility functions library. Stuff missing from std:: namespace."
   homepage "https://github.com/cppfw/utki"
-  url "https://github.com/cppfw/utki/archive/1.1.235.tar.gz"
-  sha256 "2996480ff19f343bab83b1c68ea939e614a13b8b148eb1743191ae832d5908c2"
+  url "https://github.com/cppfw/utki/archive/1.1.236.tar.gz"
+  sha256 "2ce572177ca148678da2e1bc5cb77f76fd4a810ad0b8ce0cc660a94bf80a83ac"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
@@ -11,11 +11,11 @@ class Libutki < Formula
   def install
     ENV['PATH'] += ':/usr/local/bin'
     # install from src directory because tests require submodules
-    system "/usr/local/opt/make/libexec/gnubin/make", "--directory", "src", "install", "PREFIX=#{prefix}", "lint=off"
+    system "gmake", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "--directory=src", "install", "PREFIX=#{prefix}", "lint=off"
   end
 
   test do
 	# tests require submodules, so skip those in homebrew recepie
-#    system "/usr/local/opt/make/libexec/gnubin/make", "test"
+#    system "gmake", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "test"
   end
 end
