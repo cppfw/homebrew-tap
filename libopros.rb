98d7eeb022bb9ebc641800d69f0df7c305fbde53
Anka <runner@Mac-1714330055383.local>
2024-04-28 19:28:32 +0000

    version 1.0.95 of libopros.rb

--- libopros.rb
+++ libopros.rb
@@ -1,10 +1,11 @@
 class Libopros < Formula
   desc "C++ cross-platform event waiting library."
   homepage "https://github.com/cppfw/opros"
-  url "https://github.com/cppfw/opros/archive/1.0.94.tar.gz"
-  sha256 "fba08873064d4472f9744ab7fa8f69c38322a4a98eb02c478ed6db194dca18e0"
+  url "https://github.com/cppfw/opros/archive/1.0.95.tar.gz"
+  sha256 "4456faeae416f6e50f19028598ad52a6cc421ee608f62a5f1886c83abe94d18b"
 
   depends_on "prorab" => :build
+  depends_on "prorab-extra" => :build
   depends_on "libutki"
 
   # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
