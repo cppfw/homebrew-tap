025a019f5ac33adf57ec05e7b1424d3e470dcf1c
Anka <runner@Mac-1714332259811.local>
2024-04-28 19:53:49 +0000

    version 1.0.97 of libopros.rb

--- libopros.rb
+++ libopros.rb
@@ -1,8 +1,8 @@
 class Libopros < Formula
   desc "C++ cross-platform event waiting library."
   homepage "https://github.com/cppfw/opros"
-  url "https://github.com/cppfw/opros/archive/1.0.96.tar.gz"
-  sha256 "304e35b750bacad5c04564607329fd48bc605079584ce85c7bb64d1a77969bc4"
+  url "https://github.com/cppfw/opros/archive/1.0.97.tar.gz"
+  sha256 "6bb38254fc4a6f477739ed419d35be33f5852341f3b2cf3eaa66404f98cf79ec"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
@@ -10,7 +10,7 @@ class Libopros < Formula
 
   # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
   def install
-    ENV['PATH'] += ':#{ENV['HOMEBREW_PREFIX']}/bin'
+    ENV['PATH'] += ":#{ENV['HOMEBREW_PREFIX']}/bin"
     system "#{ENV['HOMEBREW_PREFIX']}/opt/make/libexec/gnubin/make", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "install", "PREFIX=#{prefix}", "lint=off"
   end
 
