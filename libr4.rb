942fcd3f62f9608e8a641ce6623648e4e6997dc2
Anka <runner@Mac-1714418991473.local>
2024-04-29 19:48:14 +0000

    version 1.0.86 of libr4.rb

--- libr4.rb
+++ libr4.rb
@@ -1,8 +1,8 @@
 class Libr4 < Formula
   desc "C++ 3d math library."
   homepage "https://github.com/cppfw/r4"
-  url "https://github.com/cppfw/r4/archive/1.0.82.tar.gz"
-  sha256 "88957983830c496a1f982585885033a32841362f83224896f259d1c6b6e0db4e"
+  url "https://github.com/cppfw/r4/archive/1.0.86.tar.gz"
+  sha256 "2293acba87e086e5237aa993d35cbd3bd674c8c68585353de3c6d9410588fb3c"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
@@ -11,11 +11,11 @@ class Libr4 < Formula
 
   # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
   def install
-    ENV['PATH'] += ':/usr/local/bin'
-    system "/usr/local/opt/make/libexec/gnubin/make", "install", "PREFIX=#{prefix}", "lint=off"
+    ENV['PATH'] += ":#{ENV['HOMEBREW_PREFIX']}/bin"
+    system "#{ENV['HOMEBREW_PREFIX']}/opt/make/libexec/gnubin/make", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "install", "PREFIX=#{prefix}", "lint=off"
   end
 
   test do
-    system "/usr/local/opt/make/libexec/gnubin/make", "test"
+    system "#{ENV['HOMEBREW_PREFIX']}/opt/make/libexec/gnubin/make", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "test"
   end
 end
