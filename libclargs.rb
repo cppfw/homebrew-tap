c659a2023b657ee2612399080d3fb87c2ff9a068
Anka <runner@Mac-1714417764897.local>
2024-04-29 19:22:17 +0000

    version 0.2.63 of libclargs.rb

--- libclargs.rb
+++ libclargs.rb
@@ -1,8 +1,8 @@
 class Libclargs < Formula
   desc "C++ cross-platform library for parsing command line arguments."
   homepage "https://github.com/cppfw/clargs"
-  url "https://github.com/cppfw/clargs/archive/0.2.62.tar.gz"
-  sha256 "5c9808418e446607004f2c1194d298c6c089c021d4226ff976e9894d14586b98"
+  url "https://github.com/cppfw/clargs/archive/0.2.63.tar.gz"
+  sha256 "f7b3af68427b9a62d8b100f9dd4be0b591c3b75d617cad8f2c054856c988f6c8"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
@@ -11,7 +11,7 @@ class Libclargs < Formula
 
   # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
   def install
-    ENV['PATH'] += ':#{ENV['HOMEBREW_PREFIX']}/bin'
+    ENV['PATH'] += ":#{ENV['HOMEBREW_PREFIX']}/bin"
     system "#{ENV['HOMEBREW_PREFIX']}/opt/make/libexec/gnubin/make", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "--directory=src", "install", "PREFIX=#{prefix}", "lint=off"
   end
 
