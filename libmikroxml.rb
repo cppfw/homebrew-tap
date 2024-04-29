10233c171a91f74b12796266b3dc984978b72d1b
Anka <runner@Mac-1714420252416.local>
2024-04-29 20:03:34 +0000

    version 0.1.62 of libmikroxml.rb

--- libmikroxml.rb
+++ libmikroxml.rb
@@ -1,8 +1,8 @@
 class Libmikroxml < Formula
   desc "C++ cross-platform XML parser library."
   homepage "https://github.com/cppfw/mikroxml"
-  url "https://github.com/cppfw/mikroxml/archive/0.1.61.tar.gz"
-  sha256 "5b1c334e9577ab89f1ca2dd37390d3177815519af17e56c4ce7a47900dd5e059"
+  url "https://github.com/cppfw/mikroxml/archive/0.1.62.tar.gz"
+  sha256 "3e8a8780a0857e1681d540df767bc81ae013ef7dc344007a4c9b3c75e9bd8c5e"
 
   depends_on "prorab" => :build
   depends_on "pkg-config" => :build
@@ -12,11 +12,11 @@ class Libmikroxml < Formula
 
   # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
   def install
-    ENV['PATH'] += ':/usr/local/bin'
-    system "/usr/local/opt/make/libexec/gnubin/make", "install", "PREFIX=#{prefix}", "lint=off"
+    ENV['PATH'] += ":#{ENV['HOMEBREW_PREFIX']}/bin"
+    system "#{ENV['HOMEBREW_PREFIX']}/opt/make/libexec/gnubin/make", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "install", "PREFIX=#{prefix}", "lint=off"
   end
 
   test do
-    system "/usr/local/opt/make/libexec/gnubin/make", "test"
+    system "#{ENV['HOMEBREW_PREFIX']}/opt/make/libexec/gnubin/make", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "test"
   end
 end
