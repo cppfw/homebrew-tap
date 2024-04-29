730678efa480625957c3be4b555fccb1c16678c5
Anka <runner@Mac-1714418814004.local>
2024-04-29 19:36:37 +0000

    version 1.0.139 of libpapki.rb

--- libpapki.rb
+++ libpapki.rb
@@ -1,8 +1,8 @@
 class Libpapki < Formula
   desc "C++ cross-platform file system library."
   homepage "https://github.com/cppfw/papki"
-  url "https://github.com/cppfw/papki/archive/1.0.138.tar.gz"
-  sha256 "13b6bf3172d2f7d9fb35a0b0931d1d625afe8f89ed21f64dbedfe3e42cb1a4cc"
+  url "https://github.com/cppfw/papki/archive/1.0.139.tar.gz"
+  sha256 "557cd4147021e359dbceed02ae39b98575735b880088d0c5e3dcd42d3b469409"
 
   depends_on "prorab" => :build
   depends_on "libutki"
@@ -10,11 +10,11 @@ class Libpapki < Formula
   
   # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
   def install
-    ENV['PATH'] += ':/usr/local/bin'
-    system "/usr/local/opt/make/libexec/gnubin/make", "--directory", "src", "install", "PREFIX=#{prefix}", "lint=off"
+    ENV['PATH'] += ":#{ENV['HOMEBREW_PREFIX']}/bin"
+    system "#{ENV['HOMEBREW_PREFIX']}/opt/make/libexec/gnubin/make", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "--directory=src", "install", "PREFIX=#{prefix}", "lint=off"
   end
 
   test do
-    system "/usr/local/opt/make/libexec/gnubin/make", "test"
+    system "#{ENV['HOMEBREW_PREFIX']}/opt/make/libexec/gnubin/make", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "test"
   end
 end
