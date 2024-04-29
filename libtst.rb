801679444d71d344e01889a8ea1bc5aa2a3db4ee
Anka <runner@Mac-1714417775715.local>
2024-04-29 19:28:22 +0000

    version 0.3.41 of libtst.rb

--- libtst.rb
+++ libtst.rb
@@ -1,8 +1,8 @@
 class Libtst < Formula
   desc "C++ testing framework."
   homepage "https://github.com/cppfw/tst"
-  url "https://github.com/cppfw/tst/archive/0.3.40.tar.gz"
-  sha256 "6ec8568368491f73aeada867aedc964d89f07838ad8b7e400dc7e9cfe7461626"
+  url "https://github.com/cppfw/tst/archive/0.3.41.tar.gz"
+  sha256 "0fa2894e5c1126948348b49ab52461967a56849c267e26a987e3a0fb87c84c97"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
@@ -14,11 +14,11 @@ class Libtst < Formula
 
   # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
   def install
-    ENV['PATH'] += ':/usr/local/bin'
-    system "/usr/local/opt/make/libexec/gnubin/make", "install", "PREFIX=#{prefix}", "lint=off"
+    ENV['PATH'] += ":#{ENV['HOMEBREW_PREFIX']}/bin"
+    system "#{ENV['HOMEBREW_PREFIX']}/opt/make/libexec/gnubin/make", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "install", "PREFIX=#{prefix}", "lint=off"
   end
 
   test do
-    system "/usr/local/opt/make/libexec/gnubin/make", "test"
+    system "#{ENV['HOMEBREW_PREFIX']}/opt/make/libexec/gnubin/make", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "test"
   end
 end
