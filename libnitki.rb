c0e7ff287e0d7d6bb0c4c872119796b5d8c59c2d
Anka <runner@Mac-1714417532572.local>
2024-04-29 19:16:29 +0000

    version 1.0.86 of libnitki.rb

--- libnitki.rb
+++ libnitki.rb
@@ -1,8 +1,8 @@
 class Libnitki < Formula
   desc "C++ cross-platform threading library."
   homepage "https://github.com/cppfw/nitki"
-  url "https://github.com/cppfw/nitki/archive/1.0.85.tar.gz"
-  sha256 "dad4cf7e73c67a3ac14c8a7ba7fb02b0088f795f7670874924ed988b7003a89c"
+  url "https://github.com/cppfw/nitki/archive/1.0.86.tar.gz"
+  sha256 "4a5f43f295df82b080dc73b10c5cf0e885b59bb2ba33cbcbe93b7ed32b6c5466"
 
   depends_on "prorab" => :build
   depends_on "libopros"
@@ -10,7 +10,7 @@ class Libnitki < Formula
 
   # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
   def install
-    ENV['PATH'] += ':#{ENV['HOMEBREW_PREFIX']}/bin'
+    ENV['PATH'] += ":#{ENV['HOMEBREW_PREFIX']}/bin"
     system "#{ENV['HOMEBREW_PREFIX']}/opt/make/libexec/gnubin/make", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "install", "PREFIX=#{prefix}", "lint=off"
   end
 
