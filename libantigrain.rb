69ef39904dfaff5f24a15235ed2768ea5a8635cc
Anka <runner@Mac-1714423266854.local>
2024-04-29 20:53:51 +0000

    version 2.8.25 of libantigrain.rb

--- libantigrain.rb
+++ libantigrain.rb
@@ -1,8 +1,8 @@
 class Libantigrain < Formula
   desc "Anti-grain geometry C++ library."
   homepage "https://github.com/cppfw/agg"
-  url "https://github.com/cppfw/agg/archive/2.8.24.tar.gz"
-  sha256 "af9d544cb8ce1edb31edf990dba140dbac9f09fb979cf8193b2c6b232cc1d28c"
+  url "https://github.com/cppfw/agg/archive/2.8.25.tar.gz"
+  sha256 "d2e5c30ed2b7228ea941ebdd758b2889e8d6d0078e64ea4881f0efe2d5161caf"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
@@ -10,11 +10,11 @@ class Libantigrain < Formula
 
   # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
   def install
-    ENV['PATH'] += ':/usr/local/bin'
-    system "/usr/local/opt/make/libexec/gnubin/make", "install", "PREFIX=#{prefix}"
+    ENV['PATH'] += ":#{ENV['HOMEBREW_PREFIX']}/bin"
+    system "#{ENV['HOMEBREW_PREFIX']}/opt/make/libexec/gnubin/make", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "install", "PREFIX=#{prefix}", "lint=off"
   end
 
   test do
-    system "/usr/local/opt/make/libexec/gnubin/make", "test"
+    system "#{ENV['HOMEBREW_PREFIX']}/opt/make/libexec/gnubin/make", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "test"
   end
 end
