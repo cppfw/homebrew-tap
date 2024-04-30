df3222de34bebe4677a06bb8af8262c54c10a732
Anka <runner@Mac-1714481761423.local>
2024-04-30 13:10:07 +0000

    version 0.2.18 of libtml.rb

--- libtml.rb
+++ libtml.rb
@@ -1,8 +1,8 @@
 class Libtml < Formula
   desc "C++ cross-platform tml parser library."
   homepage "https://github.com/cppfw/tml"
-  url "https://github.com/cppfw/tml/archive/0.2.17.tar.gz"
-  sha256 "a244d64b9a0204f4ef988f1699d9ce3286e2d6fcc30763afbaa78b34232d20ae"
+  url "https://github.com/cppfw/tml/archive/0.2.18.tar.gz"
+  sha256 "e9bf9e6b024c402d22a4dfd2a02a1aed5c2677bdfd53f5925eed91b8c6ed251b"
 
   depends_on "prorab" => :build
   depends_on "libclargs" => :build
@@ -12,11 +12,11 @@ class Libtml < Formula
 
   # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
   def install
-    ENV['PATH'] += ':/usr/local/bin'
-    system "/usr/local/opt/make/libexec/gnubin/make", "install", "PREFIX=#{prefix}"
+    ENV['PATH'] += ":#{ENV['HOMEBREW_PREFIX']}/bin"
+    system "#{ENV['HOMEBREW_PREFIX']}/opt/make/libexec/gnubin/make", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "install", "PREFIX=#{prefix}", "lint=off"
   end
 
   test do
-    system "/usr/local/opt/make/libexec/gnubin/make", "test"
+    system "#{ENV['HOMEBREW_PREFIX']}/opt/make/libexec/gnubin/make", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "test"
   end
 end
