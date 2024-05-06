3cbdcc0fb892f616913d2b3f8cd76ca3402957aa
Anka <runner@Mac-1714993892819.local>
2024-05-06 11:31:10 +0000

    version 0.3.45 of libaudout.rb

--- libaudout.rb
+++ libaudout.rb
@@ -1,20 +1,20 @@
 class Libaudout < Formula
   desc "C++ cross-platform audio output library."
   homepage "https://github.com/cppfw/audout"
-  url "https://github.com/cppfw/audout/archive/0.3.44.tar.gz"
-  sha256 "04f3f1cb3d59bffd1e54865b6bb8ec366eddbe07c4698df649f2a3bfe2a95f11"
+  url "https://github.com/cppfw/audout/archive/0.3.45.tar.gz"
+  sha256 "d1d6f21d25ab327f3a6a146f9b49ac16fe83d75dea79e3103d787af28439e12d"
 
   depends_on "prorab" => :build
   depends_on "libnitki" => :build
   depends_on "libutki"
 
-  # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81  
+  # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
   def install
-    ENV['PATH'] += ':/usr/local/bin'
-    system "/usr/local/opt/make/libexec/gnubin/make", "install", "PREFIX=#{prefix}"
+    ENV['PATH'] += ":#{ENV['HOMEBREW_PREFIX']}/bin"
+    system "#{ENV['HOMEBREW_PREFIX']}/opt/make/libexec/gnubin/make", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "install", "PREFIX=#{prefix}", "lint=off"
   end
 
   test do
-    system "/usr/local/opt/make/libexec/gnubin/make", "test"
+    system "#{ENV['HOMEBREW_PREFIX']}/opt/make/libexec/gnubin/make", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "test"
   end
 end
