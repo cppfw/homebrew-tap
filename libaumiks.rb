a2053befaa53fd6a2b75d94961421b0952ef0e53
Anka <runner@Mac-1714994516911.local>
2024-05-06 11:42:14 +0000

    version 0.3.39 of libaumiks.rb

--- libaumiks.rb
+++ libaumiks.rb
@@ -1,8 +1,8 @@
 class Libaumiks < Formula
   desc "C++ cross-platform audio mixer library."
   homepage "https://github.com/cppfw/aumiks"
-  url "https://github.com/cppfw/aumiks/archive/0.3.38.tar.gz"
-  sha256 "d0e61d7e63fb92cff182fe6944bf4ace9653bf5e2ee06a5be84ab31dc838c744"
+  url "https://github.com/cppfw/aumiks/archive/0.3.39.tar.gz"
+  sha256 "007b84632ee005b585b46c7cab59b3206848ebbadc168452465b0697791f7ca4"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
@@ -12,11 +12,11 @@ class Libaumiks < Formula
 
   # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
   def install
-    ENV['PATH'] += ':/usr/local/bin'
-    system "/usr/local/opt/make/libexec/gnubin/make", "install", "PREFIX=#{prefix}"
+    ENV['PATH'] += ":#{ENV['HOMEBREW_PREFIX']}/bin"
+    system "#{ENV['HOMEBREW_PREFIX']}/opt/make/libexec/gnubin/make", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "install", "PREFIX=#{prefix}", "lint=off"
   end
 
-  #test do
-  #  system "/usr/local/opt/make/libexec/gnubin/make", "test"
-  #end
+  test do
+    system "#{ENV['HOMEBREW_PREFIX']}/opt/make/libexec/gnubin/make", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "test"
+  end
 end
