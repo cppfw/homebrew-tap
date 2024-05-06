bd4c0c38ea727848e244a3c74ede3e5482c2b8b6
Anka <runner@Mac-1714995242909.local>
2024-05-06 11:52:49 +0000

    version 0.1.8 of liburlmodel.rb

--- liburlmodel.rb
+++ liburlmodel.rb
@@ -1,8 +1,8 @@
 class Liburlmodel < Formula
   desc "URL model C++ library."
   homepage "https://github.com/cppfw/urlmodel"
-  url "https://github.com/cppfw/urlmodel/archive/0.1.7.tar.gz"
-  sha256 "0ad4f3ea7d1f5f9be6e1824af26d36ae1f0c94d4a0afeadc1d6633f7e594841a"
+  url "https://github.com/cppfw/urlmodel/archive/0.1.8.tar.gz"
+  sha256 "bc9229fd97308ff03cbd417c0f9fcb6d165908db7eff31ae83fd368a9bb3c867"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
@@ -11,12 +11,11 @@ class Liburlmodel < Formula
 
   # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
   def install
-    ENV['PATH'] += ':/usr/local/bin'
-    # install from src directory because tests require submodules
-    system "/usr/local/opt/make/libexec/gnubin/make", "--directory", "src", "install", "PREFIX=#{prefix}", "lint=off"
+    ENV['PATH'] += ":#{ENV['HOMEBREW_PREFIX']}/bin"
+    system "#{ENV['HOMEBREW_PREFIX']}/opt/make/libexec/gnubin/make", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "--directory=src", "install", "PREFIX=#{prefix}", "lint=off"
   end
 
   test do
-    system "/usr/local/opt/make/libexec/gnubin/make", "test"
+    system "#{ENV['HOMEBREW_PREFIX']}/opt/make/libexec/gnubin/make", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "test"
   end
 end
