ca496c2de5e93560f0726713777bd7c37bed3bbb
Anka <runner@Mac-1715949009035.local>
2024-05-17 12:44:13 +0000

    version 1.1.239 of libutki.rb

--- libutki.rb
+++ libutki.rb
@@ -1,15 +1,15 @@
 class Libutki < Formula
   desc "C++ utility functions library. Stuff missing from std:: namespace."
   homepage "https://github.com/cppfw/utki"
-  url "https://github.com/cppfw/utki/archive/1.1.238.tar.gz"
-  sha256 "af11f8ae042ff9b6bf17216319a25292b3e9e2858812e4f7292bcde9f381bf4e"
+  url "https://github.com/cppfw/utki/archive/1.1.239.tar.gz"
+  sha256 "31f9c6beb27a96a6d8b51d5a3c80ab2e6afeeb9f9d5ac096f03980cb4b83fe85"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
 
   # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
   def install
-    ENV['PATH'] += ':/usr/local/bin'
+    ENV['PATH'] += ":#{ENV['HOMEBREW_PREFIX']}/bin"
     # install from src directory because tests require submodules
     system "#{ENV['HOMEBREW_PREFIX']}/opt/make/libexec/gnubin/make", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "--directory=src", "install", "PREFIX=#{prefix}", "lint=off"
   end
