c2c392d57cd1b3a7ca5934e5cf89ffad70d38858
Anka <runner@Mac-1716381868290.local>
2024-05-22 12:55:22 +0000

    version 2.0.25 of prorab.rb

--- prorab.rb
+++ prorab.rb
@@ -1,19 +1,19 @@
 class Prorab < Formula
   desc "GNU make based build system."
   homepage "https://github.com/cppfw/prorab"
-  url "https://github.com/cppfw/prorab/archive/2.0.24.tar.gz"
-  sha256 "eca8f8e46db271565e140018a111991a871ca7d939d0310ae885a3ef3763d0fc"
+  url "https://github.com/cppfw/prorab/archive/2.0.25.tar.gz"
+  sha256 "0159e8a6c0ff3cc3c437acf3efbfa0c821ae6f06277480bf10f9056a083ab470"
 
   depends_on "dos2unix" => :build
   depends_on "make"
 
   # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
   def install
-    ENV['PATH'] += ':/usr/local/bin'
-    system "gmake", "--include-dir=$(brew --prefix)/include", "install", "PREFIX=#{prefix}"
+    ENV['PATH'] += ":#{ENV['HOMEBREW_PREFIX']}/bin"
+    system "#{ENV['HOMEBREW_PREFIX']}/opt/make/libexec/gnubin/make", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "install", "PREFIX=#{prefix}", "lint=off"
   end
   
   test do
-    system "gmake", "--include-dir=$(brew --prefix)/include", "test"
+    system "#{ENV['HOMEBREW_PREFIX']}/opt/make/libexec/gnubin/make", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "test"
   end
 end
