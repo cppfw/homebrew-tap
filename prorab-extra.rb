b1f181c4ec964e839f61675f3dcfc7e454bcee7c
Anka <runner@Mac-1716992411187.local>
2024-05-29 14:33:38 +0000

    version 0.2.52 of prorab-extra.rb

--- prorab-extra.rb
+++ prorab-extra.rb
@@ -1,8 +1,8 @@
 class ProrabExtra < Formula
   desc "GNU make based build system."
   homepage "https://github.com/cppfw/prorab-extra"
-  url "https://github.com/cppfw/prorab-extra/archive/0.2.51.tar.gz"
-  sha256 "8f027b3d03a440a1ec999dbdce6259989d9cd42f2ee536c425986eece940f447"
+  url "https://github.com/cppfw/prorab-extra/archive/0.2.52.tar.gz"
+  sha256 "92fc43c7b048ef07d841c1941db0efa6def85ba82e79d3e6e19f48079c5c126c"
 
   depends_on "make"
   depends_on "myci"
@@ -10,11 +10,11 @@ class ProrabExtra < Formula
 
   # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
   def install
-    ENV['PATH'] += ':/usr/local/bin'
-    system "gmake", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "install", "PREFIX=#{prefix}"
+    ENV['PATH'] += ":#{ENV['HOMEBREW_PREFIX']}/bin"
+    system "#{ENV['HOMEBREW_PREFIX']}/opt/make/libexec/gnubin/make", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "install", "PREFIX=#{prefix}", "lint=off"
   end
   
   test do
-    system "gmake", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "test"
+    system "#{ENV['HOMEBREW_PREFIX']}/opt/make/libexec/gnubin/make", "--include-dir=#{ENV['HOMEBREW_PREFIX']}/include", "test"
   end
 end
