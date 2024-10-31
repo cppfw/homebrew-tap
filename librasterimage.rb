6aaf68289087a6db3085370ca7237732203bc540
Anka <runner@Mac-1730378133580.local>
2024-10-31 12:46:03 +0000

    version 0.1.19 of librasterimage.rb

--- librasterimage.rb
+++ librasterimage.rb
@@ -1,8 +1,8 @@
 class Librasterimage < Formula
   desc "C++ cross-platform SVG rendering library."
   homepage "https://github.com/igagis/rasterimage"
-  url "https://github.com/igagis/rasterimage/archive/0.1.18.tar.gz"
-  sha256 "59b6fcf18daf72cd9512cf60b1793c63439307693c17fca2fe04ac71aef1f232"
+  url "https://github.com/igagis/rasterimage/archive/0.1.19.tar.gz"
+  sha256 "bc7b468c37822ba747c1a52ecf088cd6f73f2da1ccff35a18e8d6d57a94e1d27"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
