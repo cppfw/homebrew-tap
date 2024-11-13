762def2b9c8199e415afec0135b48985d793611c
Anka <runner@Mac-1731494824302.local>
2024-11-13 10:58:03 +0000

    version 0.6.20 of libsvgren.rb

--- libsvgren.rb
+++ libsvgren.rb
@@ -1,13 +1,13 @@
 class Libsvgren < Formula
   desc "C++ cross-platform SVG rendering library."
-  homepage "https://github.com/igagis/svgren"
-  url "https://github.com/igagis/svgren/archive/0.6.19.tar.gz"
-  sha256 "19a73c1c0d00de5051558b5260bcacc02b2c6aae39d18d3f0b81de8d8d5ea201"
+  homepage "https://github.com/cppfw/svgren"
+  url "https://github.com/cppfw/svgren/archive/0.6.20.tar.gz"
+  sha256 "c95358d286b960fccc0a33a9a2e9134ef3ad2cb529bc03bd60d965b280d42248"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
   depends_on "myci" => :build
-  depends_on "libantigrain" => :build
+  depends_on "libveg" => :build
   depends_on "libr4" => :build
   depends_on "libclargs" => :build
   depends_on "libtst" => :build
