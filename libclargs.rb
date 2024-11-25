92fccc6952676cbc9e9f0b701a59d4f4b757028b
Anka <runner@Mac-1732566823216.local>
2024-11-25 20:44:25 +0000

    version 0.2.66 of libclargs.rb

--- libclargs.rb
+++ libclargs.rb
@@ -1,8 +1,8 @@
 class Libclargs < Formula
   desc "C++ cross-platform library for parsing command line arguments."
   homepage "https://github.com/cppfw/clargs"
-  url "https://github.com/cppfw/clargs/archive/0.2.65.tar.gz"
-  sha256 "c0baea086e3a6eb99b0dbb606b0d372fd711f50e70d28291d9e2d7bdaf8525d0"
+  url "https://github.com/cppfw/clargs/archive/0.2.66.tar.gz"
+  sha256 "ca8372b4000dc69441fcd704d308d8c8430e4a0340355ab19fc01f5f3b189c01"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
