0ee5e56e0dfc52d462085fcb8d4cb532316d883e
Anka <runner@Mac-1740518065632.local>
2025-02-25 21:33:58 +0000

    version 0.5.258 of libruis.rb

--- libruis.rb
+++ libruis.rb
@@ -1,8 +1,8 @@
 class Libruis < Formula
   desc "C++ cross-platform OpenGL based GUI library."
   homepage "https://github.com/cppfw/ruis"
-  url "https://github.com/cppfw/ruis/archive/0.5.257.tar.gz"
-  sha256 "443329059ba1c64889317bc314c110523d076340000fc0d861fb2cfd655b5590"
+  url "https://github.com/cppfw/ruis/archive/0.5.258.tar.gz"
+  sha256 "38563d161788c55d9caf62045d97bd5f2f7579fdc544ee02eb1c21004b7408a9"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
