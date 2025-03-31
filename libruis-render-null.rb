995bc7a145675d00b26022c1600a3e441216b3ce
Anka <runner@Mac-1743461012594.local>
2025-03-31 23:02:46 +0000

    version 0.0.8 of libruis-render-null.rb

--- libruis-render-null.rb
+++ libruis-render-null.rb
@@ -1,8 +1,8 @@
 class LibruisRenderNull < Formula
   desc "OpenGl renderer for ruis GUI library."
   homepage "https://github.com/cppfw/ruis-render-null"
-  url "https://github.com/cppfw/ruis-render-null/archive/0.0.7.tar.gz"
-  sha256 "253c431c97e7f4a3e797e3b0c67c6e0353ccc4484feba6b2f60c82f26e67b747"
+  url "https://github.com/cppfw/ruis-render-null/archive/0.0.8.tar.gz"
+  sha256 "19aff72d278e13f1c5bd61cb0e951fcda46ab99fd87466373c4c1b685e07ff41"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
