8f264d28ff306cdd600de94cca4b2b11d2bdd6d4
Anka <runner@Mac-1751394249451.local>
2025-07-01 18:35:42 +0000

    version 0.3.48 of

--- libtst.rb
+++ libtst.rb
@@ -1,8 +1,8 @@
 class Libtst < Formula
   desc "C++ testing framework."
   homepage "https://github.com/cppfw/tst"
-  url "https://github.com/cppfw/tst/archive/0.3.47.tar.gz"
-  sha256 "f70509d3910e562d75d4f7d9be7e7b198006c0f356994945869bad0b80e7c4b3"
+  url "https://github.com/cppfw/tst/archive/0.3.48.tar.gz"
+  sha256 "b27f82ed1024a878b136b72c60dfc56ab5cbec00e0bc3875431ec7c8d3a3f874"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
