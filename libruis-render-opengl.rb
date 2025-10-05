f87b54249e990d0ccc9f10d79cffab82f11ec014
Anka <runner@Mac-1759684090682.local>
2025-10-05 17:39:28 +0000

    version 0.3.0 of

--- libruis-render-opengl.rb
+++ libruis-render-opengl.rb
@@ -1,8 +1,8 @@
 class LibruisRenderOpengl < Formula
   desc "OpenGl renderer for ruis GUI library."
   homepage "https://github.com/cppfw/ruis-render-opengl"
-  url "https://github.com/cppfw/ruis-render-opengl/archive/0.1.93.tar.gz"
-  sha256 "2cd98ef840adb1605a1f30e1cdae54b4fcb62968842b82454ad9e68ec0eb1216"
+  url "https://github.com/cppfw/ruis-render-opengl/archive/0.3.0.tar.gz"
+  sha256 "9246c3622c0e3c51ea8d5a644a553387f49c55958f457a01b6c2e664ab407a34"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
