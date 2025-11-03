57852819c60c95f1704a1360bfc6d9bac85e60f4
Anka <runner@iad20-fj925-b2e10b79-5e28-4b9f-aaf0-7c743e8e7fb3-82CFA07E3B0F.local>
2025-11-03 12:18:39 +0000

    version 0.1.36 of libjsondom.rb

--- libjsondom.rb
+++ libjsondom.rb
@@ -1,15 +1,15 @@
 class Libjsondom < Formula
   desc "C++ cross-platform JSON DOM library."
   homepage "https://github.com/cppfw/jsondom"
-  url "https://github.com/cppfw/jsondom/archive/0.1.35.tar.gz"
-  sha256 "894bad58b0a46c3b696e4a287a0d1eee0f2f78069e0f556b09709025ddf5f7ba"
+  url "https://github.com/cppfw/jsondom/archive/0.1.36.tar.gz"
+  sha256 "ca157a335a3cad874310065137686aefea5c308e82e03e9769b1ad0940aa6866"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
   depends_on "myci" => :build
   depends_on "pkg-config" => :build
   depends_on "libtst" => :build
-  depends_on "libpapki"
+  depends_on "libfsif"
   depends_on "libutki"
 
   # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
