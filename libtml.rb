2114f263f2af70576122f2053050e358daeff70c
Anka <runner@sat12-bq146-e39a6042-929e-4253-bb16-5297199ce6e4-8E866373DAE7.local>
2025-11-03 12:35:53 +0000

    version 0.2.33 of libtml.rb

--- libtml.rb
+++ libtml.rb
@@ -1,13 +1,13 @@
 class Libtml < Formula
   desc "C++ cross-platform tml parser library."
   homepage "https://github.com/cppfw/tml"
-  url "https://github.com/cppfw/tml/archive/0.2.32.tar.gz"
-  sha256 "29e5c3293d063676799c691af7dbd676a7ed1ef1571b40841c50b16a38ef8f83"
+  url "https://github.com/cppfw/tml/archive/0.2.33.tar.gz"
+  sha256 "8afa47b8c18f6dcaeff8ad0119aa805c6e89ecdd6fb48870cf40d5f85712db2d"
 
   depends_on "prorab" => :build
   depends_on "libclargs" => :build
   depends_on "libtst" => :build
-  depends_on "libpapki"
+  depends_on "libfsif"
   depends_on "libutki"
 
   # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
