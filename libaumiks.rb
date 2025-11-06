b3cb32999e48e9163d22ee0bd71b606aa85b1bb5
Anka <runner@sat12-bq165-7619ed11-1fb2-4a9c-9320-e7f7b7fb35c7-BE70CDBF7779.local>
2025-11-06 13:26:04 +0000

    version 0.3.44 of libaumiks.rb

--- libaumiks.rb
+++ libaumiks.rb
@@ -1,14 +1,14 @@
 class Libaumiks < Formula
   desc "C++ cross-platform audio mixer library."
   homepage "https://github.com/cppfw/aumiks"
-  url "https://github.com/cppfw/aumiks/archive/0.3.43.tar.gz"
-  sha256 "7c6296479ad38c111a85c08f05a969aa506381d9aae834148c060bc7541416f3"
+  url "https://github.com/cppfw/aumiks/archive/0.3.44.tar.gz"
+  sha256 "7b6188327287edbf18fb1a3bea3f5b4e22beb94e7c0252e3ea7dac3793cc8c4e"
 
   depends_on "prorab" => :build
   depends_on "prorab-extra" => :build
   depends_on "myci" => :build
   depends_on "libaudout"
-  depends_on "libpapki"
+  depends_on "libfsif"
 
   # use gmake here because otherwise homebrew uses default Mac's make which is of too old version 3.81
   def install
