9dabc9e684c311d457322dc529079e27b58a3906
Anka <runner@iad20-eo1205-7f43ad55-0957-4f70-9455-77a6d7e37b44-DA9CA359F498.local>
2026-04-16 20:38:44 +0000

    version 0.1.226 of myci.rb

--- myci.rb
+++ myci.rb
@@ -1,8 +1,8 @@
 class Myci < Formula
   desc "Helper CI scripts."
   homepage "https://github.com/cppfw/myci"
-  url "https://github.com/cppfw/myci/archive/0.1.225.tar.gz"
-  sha256 "31468c6fd93147a3707f3162d1a97ae52872a12808b464785c520fe75f9daf03"
+  url "https://github.com/cppfw/myci/archive/0.1.226.tar.gz"
+  sha256 "129dbc055cce8bdd11a7572b78e9a7bf05e639b82764996d39f5ae4db6af342f"
 
   depends_on "md5sha1sum"
   depends_on "curl"
