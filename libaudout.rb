fce04a57b4847c9173c347e98f9508c4fc956453
Anka <runner@sat12-dp146-b9055ccc-bccf-4003-ad1c-18166b8318c5-C2F5DD76DB89.local>
2026-09-18 14:00:47 +0000

    version 0.3.50 of libaudout.rb

--- libaudout.rb
+++ libaudout.rb
@@ -1,8 +1,8 @@
 class Libaudout < Formula
   desc "C++ cross-platform audio output library."
   homepage "https://github.com/cppfw/audout"
-  url "https://github.com/cppfw/audout/archive/0.3.49.tar.gz"
-  sha256 "a6623c5791da4112ead274b19a39c91936b51339f173890f9ad2901137131183"
+  url "https://github.com/cppfw/audout/archive/0.3.50.tar.gz"
+  sha256 "a1dd2f386b07d6f425f7c8c063d7dddd0683fdbb486ed4f4f21106416bbe2220"
 
   depends_on "prorab" => :build
   depends_on "libnitki" => :build
